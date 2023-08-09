0c82cb338846cb50083a8e8e11a11e478c805d47
Anselm <anselm.eickhoff@gmail.com>
2023-08-09 14:11:22 +0100

    Better exports

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,3 @@
-import type { CoValueID, ContentType } from "./contentType";
-import type { JsonValue } from "./jsonValue";
 import {
     CoValue,
     agentCredentialFromBytes,
@@ -11,6 +9,11 @@ import {
 } from "./coValue";
 import { LocalNode } from "./node";
 import { CoMap } from "./contentTypes/coMap";
+
+import type { AgentCredential } from "./coValue";
+import type { AgentID, SessionID } from "./ids";
+import type { CoValueID, ContentType } from "./contentType";
+import type { JsonValue } from "./jsonValue";
 
 type Value = JsonValue | ContentType;
 
@@ -25,4 +28,12 @@ const internals = {
 
 export { LocalNode, CoValue, CoMap, internals };
 
-export type { Value, JsonValue, ContentType, CoValueID };
+export type {
+    Value,
+    JsonValue,
+    ContentType,
+    CoValueID,
+    AgentCredential,
+    SessionID,
+    AgentID,
+};
