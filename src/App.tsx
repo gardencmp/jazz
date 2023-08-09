6ce93bc4182c0f8f3b5fb0885452ddba6819a79b
Anselm <anselm.eickhoff@gmail.com>
2023-08-09 11:48:18 +0100

    First local working version with jazz

--- src/App.tsx
+++ src/App.tsx
@@ -7,27 +7,66 @@ import {
     TableRow,
 } from "@/components/ui/table";
 import { Input } from "@/components/ui/input";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Checkbox } from "./components/ui/checkbox";
+import { CoMap, CoValueID } from "cojson";
+import { Button } from "./components/ui/button";
+import { useJazz, useTelepathicState } from "./main";
 
-type Todo = {
-    done: boolean;
-    text: string;
-};
+type TaskContent = { done: boolean; text: string };
+type Task = CoMap<TaskContent, {}>;
+
+type TodoListContent = { title: string; [taskId: CoValueID<Task>]: true };
+type TodoList = CoMap<TodoListContent, {}>;
 
 function App() {
+    const [listId, setListId] = useState<CoValueID<TodoList>>();
+
+    const { localNode } = useJazz();
+
+    const createList = () => {
+        const listTeam = localNode.createTeam();
+        const list = listTeam.createMap<TodoListContent, {}>();
+
+        list.edit((list) => {
+            list.set("title", "My Todo List");
+        });
+
+        setListId(list.id);
+    };
+
     return (
-        <div className="flex h-full items-center justify-center">
-            <TodoList />
+        <div className="flex flex-col h-full items-center justify-center gap-10">
+            {listId && <TodoList listId={listId} />}
+            <Button onClick={createList}>Create New List</Button>
         </div>
     );
 }
 
-export function TodoList() {
-    const [todos, setTodos] = useState<Todo[]>([]);
+export function TodoList({ listId }: { listId: CoValueID<TodoList> }) {
+    const list = useTelepathicState(listId);
+
+    const createTodo = (text: string) => {
+        if (!list) return;
+        let task = list.coValue.getTeam().createMap<TaskContent, {}>();
+
+        task = task.edit((task) => {
+            task.set("text", text);
+            task.set("done", false);
+        });
+
+        console.log("Created task", task.id, task.toJSON());
+
+        const listAfter = list.edit((list) => {
+            list.set(task.id, true);
+        });
+
+        console.log("Updated list", listAfter.toJSON());
+    };
 
     return (
         <div className="max-w-full w-4xl">
+            <h1>{list?.get("title")}</h1>
             <Table>
                 <TableHeader>
                     <TableRow>
@@ -36,24 +75,14 @@ export function TodoList() {
                     </TableRow>
                 </TableHeader>
                 <TableBody>
-                    {todos.map((todo, index) => (
-                        <TableRow key={index}>
-                            <TableCell>
-                                <Checkbox
-                                    className="mt-1"
-                                    checked={todo.done}
-                                    onCheckedChange={(checked) => {
-                                        setTodos([
-                                            ...todos.slice(0, index),
-                                            { ...todo, done: !!checked },
-                                            ...todos.slice(index + 1),
-                                        ]);
-                                    }}
-                                />
-                            </TableCell>
-                            <TableCell>{todo.text}</TableCell>
-                        </TableRow>
-                    ))}
+                    {list &&
+                        list.keys()
+                            .filter((key): key is CoValueID<Task> =>
+                                key.startsWith("co_")
+                            )
+                            .map((taskId) => (
+                                <TodoRow key={taskId} taskId={taskId} />
+                            ))}
                     <TableRow key="new">
                         <TableCell>
                             <Checkbox className="mt-1" disabled />
@@ -66,10 +95,7 @@ export function TodoList() {
                                         e.currentTarget.elements.namedItem(
                                             "text"
                                         ) as HTMLInputElement;
-                                    setTodos([
-                                        ...todos,
-                                        { done: false, text: textEl.value },
-                                    ]);
+                                    createTodo(textEl.value);
                                     textEl.value = "";
                                 }}
                             >
@@ -84,6 +110,27 @@ export function TodoList() {
                 </TableBody>
             </Table>
         </div>
+    );
+}
+
+function TodoRow({ taskId }: { taskId: CoValueID<Task> }) {
+    const task = useTelepathicState(taskId);
+
+    return (
+        <TableRow>
+            <TableCell>
+                <Checkbox
+                    className="mt-1"
+                    checked={task?.get("done")}
+                    onCheckedChange={(checked) => {
+                        task?.edit((task) => {
+                            task.set("done", !!checked);
+                        });
+                    }}
+                />
+            </TableCell>
+            <TableCell>{task?.get("text")}</TableCell>
+        </TableRow>
     );
 }
 
