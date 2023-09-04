24ce7dbdf1f94a550adc1a4930433a31e6a67786
Anselm <anselm.eickhoff@gmail.com>
2023-09-04 19:15:51 +0100

    Use better ws streams

--- packages/cojson-simple-sync/src/index.ts
+++ packages/cojson-simple-sync/src/index.ts
@@ -1,8 +1,7 @@
 import { AnonymousControlledAccount, LocalNode, cojsonInternals } from "cojson";
-import { WebSocketServer, createWebSocketStream } from "ws";
-import { Duplex } from "node:stream";
-import { TransformStream } from "node:stream/web";
+import { WebSocketServer } from "ws";
 import { SQLiteStorage } from "cojson-storage-sqlite";
+import { websocketReadableStream, websocketWritableStream } from "./websocketStreams.js";
 
 const wss = new WebSocketServer({ port: 4200 });
 
@@ -35,28 +34,7 @@ wss.on("connection", function connection(ws, req) {
         clearInterval(pinging);
     });
 
-    const duplexStream = createWebSocketStream(ws, {
-        decodeStrings: false,
-        readableObjectMode: true,
-        writableObjectMode: true,
-        encoding: "utf-8",
-        defaultEncoding: "utf-8",
-    });
 
-    const { readable: incomingStrings, writable: outgoingStrings } =
-        Duplex.toWeb(duplexStream);
-
-    const toJSON = new TransformStream({
-        transform: (chunk, controller) => {
-            controller.enqueue(JSON.parse(chunk));
-        },
-    });
-
-    const fromJSON = new TransformStream({
-        transform: (chunk, controller) => {
-            controller.enqueue(JSON.stringify(chunk));
-        },
-    });
 
     const clientAddress =
         (req.headers["x-forwarded-for"] as string | undefined)
@@ -68,11 +46,9 @@ wss.on("connection", function connection(ws, req) {
     localNode.sync.addPeer({
         id: clientId,
         role: "client",
-        incoming: incomingStrings.pipeThrough(toJSON),
-        outgoing: fromJSON.writable,
+        incoming: websocketReadableStream(ws),
+        outgoing: websocketWritableStream(ws),
     });
-
-    void fromJSON.readable.pipeTo(outgoingStrings);
 
     ws.on("error", (e) => console.error(`Error on connection ${clientId}:`, e));
 });
