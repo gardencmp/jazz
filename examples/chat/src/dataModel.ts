b961cde9460589845b581e1128a92e20a8305739
Tobias Lins <me@tobi.sh>
2023-12-20 20:54:06 +0100

    Revert changed files

--- examples/chat/src/dataModel.ts
+++ examples/chat/src/dataModel.ts
@@ -1,4 +1,4 @@
-import { CoMap, CoList } from "cojson";
+import { CoMap, CoList } from 'cojson';
 
-export type Chat = CoList<Message["id"]>;
+export type Chat = CoList<Message['id']>;
 export type Message = CoMap<{ text: string }>;
--- examples/todo/src/3_NewProjectForm.tsx
+++ examples/todo/src/3_NewProjectForm.tsx
@@ -9,42 +9,41 @@ import { SubmittableInput } from "./basicComponents";
 import { useNavigate } from "react-router";
 import { Profile } from "cojson";
 
-console.log("test");
 export function NewProjectForm() {
-  // `me` represents the current user account, which will determine
-  // access rights to CoValues. We get it from the top-level provider `<WithJazz/>`.
-  const { me } = useJazz<Profile, TodoAccountRoot>();
-  const navigate = useNavigate();
+    // `me` represents the current user account, which will determine
+    // access rights to CoValues. We get it from the top-level provider `<WithJazz/>`.
+    const { me } = useJazz<Profile, TodoAccountRoot>();
+    const navigate = useNavigate();
 
-  const createProject = useCallback(
-    (title: string) => {
-      if (!title) return;
+    const createProject = useCallback(
+        (title: string) => {
+            if (!title) return;
 
-      // To create a new todo project, we first create a `Group`,
-      // which is a scope for defining access rights (reader/writer/admin)
-      // of its members, which will apply to all CoValues owned by that group.
-      const projectGroup = me.createGroup();
+            // To create a new todo project, we first create a `Group`,
+            // which is a scope for defining access rights (reader/writer/admin)
+            // of its members, which will apply to all CoValues owned by that group.
+            const projectGroup = me.createGroup();
 
-      // Then we create an empty todo project within that group
-      const project = projectGroup.createMap<TodoProject>({
-        title,
-        tasks: projectGroup.createList<ListOfTasks>().id,
-      });
+            // Then we create an empty todo project within that group
+            const project = projectGroup.createMap<TodoProject>({
+                title,
+                tasks: projectGroup.createList<ListOfTasks>().id,
+            });
 
-      me.root?.projects?.append(project.id);
+            me.root?.projects?.append(project.id);
 
-      navigate("/project/" + project.id);
-    },
-    [me, navigate]
-  );
+            navigate("/project/" + project.id);
+        },
+        [me, navigate]
+    );
 
-  return (
-    <SubmittableInput
-      onSubmit={createProject}
-      label="Create New Project"
-      placeholder="New project title"
-    />
-  );
+    return (
+        <SubmittableInput
+            onSubmit={createProject}
+            label="Create New Project"
+            placeholder="New project title"
+        />
+    );
 }
 
 /** Walkthrough: continue with ./4_ProjectTodoTable.tsx */
