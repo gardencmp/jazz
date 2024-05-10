bf22588b0ecca981bac43a28a56eeaa3dab92202
Anselm <anselm.eickhoff@gmail.com>
2024-05-10 14:41:24 +0100

    Small homepage fixes

--- homepage/homepage/components/forMdx.tsx
+++ homepage/homepage/components/forMdx.tsx
@@ -59,7 +59,7 @@ export function GridCard(props: { children: ReactNode; className?: string }) {
     return (
         <div
             className={[
-                "p-4 [&>h4]:mt-0 [&>h3]:mt-0 [&>:last-child]:mb-0",
+                "col-span-2 p-4 [&>h4]:mt-0 [&>h3]:mt-0 [&>:last-child]:mb-0",
                 "border border-stone-200 dark:border-stone-800 rounded-xl  shadow-sm",
                 props.className,
             ].join(" ")}
