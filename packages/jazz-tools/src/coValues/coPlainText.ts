85e862978be8ef1e0d54c254fcfbdc7ca9f6ed02
Anselm <anselm.eickhoff@gmail.com>
2024-06-03 16:48:18 +0100

    Progress on text

--- packages/jazz-tools/src/coValues/coPlainText.ts
+++ packages/jazz-tools/src/coValues/coPlainText.ts
@@ -3,18 +3,18 @@ import { RawAccount, stringifyOpID } from "cojson";
 import type {
     AccountCtx,
     CoValue,
+    CoValueClass,
     ID,
-    SubclassedConstructor,
     UnavailableError,
 } from "../internal.js";
-import { Account, CoValueBase, Group, inspect } from "../internal.js";
+import { Account, CoValueBase, Group, ensureCoValueLoaded, inspect, loadCoValue, loadCoValueEf, subscribeToCoValue, subscribeToCoValueEf, subscribeToExistingCoValue } from "../internal.js";
 import type { Effect, Stream } from "effect";
 
 export type TextPos = CojsonInternalTypes.OpID;
 
 export class CoPlainText
     extends String
-    implements CoValue<"CoPlainText", RawCoPlainText>
+    implements CoValue
 {
     declare id: ID<this>;
     declare _type: "CoPlainText";
@@ -48,7 +48,7 @@ export class CoPlainText
         });
     }
 
-    static create<T extends CoPlainText>(this: SubclassedConstructor<T>, text: string, options: { owner: Account | Group }) {
+    static create<T extends CoPlainText>(this: CoValueClass<T>, text: string, options: { owner: Account | Group }) {
         return new this({ text, owner: options.owner });
     }
 
@@ -92,42 +92,120 @@ export class CoPlainText
         return this._raw.mapping.idxAfterOpID[stringifyOpID(pos)];
     }
 
-    subscribe!: (listener: (update: this) => void) => () => void;
-    static {
-        this.prototype.subscribe = CoValueBase.prototype.subscribe as any;
-    }
-
-    subscribeEf!: () => Stream.Stream<this, "unavailable", never>;
-    static {
-        this.prototype.subscribeEf = CoValueBase.prototype.subscribeEf as any;
-    }
-
     static fromRaw<V extends CoPlainText>(
-        this: SubclassedConstructor<V> & typeof CoPlainText,
+        this: CoValueClass<V> & typeof CoPlainText,
         raw: RawCoPlainText
     ) {
         return new this({ fromRaw: raw });
     }
 
-    static loadEf = CoValueBase.loadEf as unknown as <V extends CoValue>(
-        this: SubclassedConstructor<V>,
-        id: ID<V>
-    ) => Effect.Effect<V, UnavailableError, AccountCtx>;
-    static load = CoValueBase.load as unknown as <V extends CoValue>(
-        this: SubclassedConstructor<V>,
-        id: ID<V>,
-        options: { as: Account | Group }
-    ) => Promise<V | undefined>;
-    static subscribeEf = CoValueBase.subscribeEf as unknown as <
-        V extends CoValue,
-    >(
-        this: SubclassedConstructor<V>,
-        id: ID<V>
-    ) => Stream.Stream<V, UnavailableError, AccountCtx>;
-    static subscribe = CoValueBase.subscribe as unknown as <V extends CoValue>(
-        this: SubclassedConstructor<V>,
-        id: ID<V>,
-        options: { as: Account | Group },
-        onUpdate: (value: V) => void
-    ) => () => void;
+    /**
+     * Load a `CoPlainText` with a given ID, as a given account.
+     *
+     * `depth` specifies which (if any) fields that reference other CoValues to load as well before resolving.
+     * The `DeeplyLoaded` return type guarantees that corresponding referenced CoValues are loaded to the specified depth.
+     *
+     * You can pass `[]` or `{}` for shallowly loading only this CoPlainText, or `{ fieldA: depthA, fieldB: depthB }` for recursively loading referenced CoValues.
+     *
+     * Check out the `load` methods on `CoMap`/`CoList`/`CoStream`/`Group`/`Account` to see which depth structures are valid to nest.
+     *
+     * @example
+     * ```ts
+     * const person = await Person.load(
+     *   "co_zdsMhHtfG6VNKt7RqPUPvUtN2Ax",
+     *   me,
+     *   { pet: {} }
+     * );
+     * ```
+     *
+     * @category Subscription & Loading
+     */
+    static load<T extends CoPlainText>(
+        this: CoValueClass<T>,
+        id: ID<T>,
+        as: Account,
+    ): Promise<T | undefined> {
+        return loadCoValue(this, id, as, []);
+    }
+
+    /**
+     * Effectful version of `CoMap.load()`.
+     *
+     * Needs to be run inside an `AccountCtx` context.
+     *
+     * @category Subscription & Loading
+     */
+    static loadEf<T extends CoPlainText>(
+        this: CoValueClass<T>,
+        id: ID<T>,
+    ): Effect.Effect<T, UnavailableError, AccountCtx> {
+        return loadCoValueEf(this, id, []);
+    }
+
+    /**
+     * Load and subscribe to a `CoMap` with a given ID, as a given account.
+     *
+     * Automatically also subscribes to updates to all referenced/nested CoValues as soon as they are accessed in the listener.
+     *
+     * `depth` specifies which (if any) fields that reference other CoValues to load as well before calling `listener` for the first time.
+     * The `DeeplyLoaded` return type guarantees that corresponding referenced CoValues are loaded to the specified depth.
+     *
+     * You can pass `[]` or `{}` for shallowly loading only this CoMap, or `{ fieldA: depthA, fieldB: depthB }` for recursively loading referenced CoValues.
+     *
+     * Check out the `load` methods on `CoMap`/`CoList`/`CoStream`/`Group`/`Account` to see which depth structures are valid to nest.
+     *
+     * Returns an unsubscribe function that you should call when you no longer need updates.
+     *
+     * Also see the `useCoState` hook to reactively subscribe to a CoValue in a React component.
+     *
+     * @example
+     * ```ts
+     * const unsub = Person.subscribe(
+     *   "co_zdsMhHtfG6VNKt7RqPUPvUtN2Ax",
+     *   me,
+     *   { pet: {} },
+     *   (person) => console.log(person)
+     * );
+     * ```
+     *
+     * @category Subscription & Loading
+     */
+    static subscribe<T extends CoPlainText>(
+        this: CoValueClass<T>,
+        id: ID<T>,
+        as: Account,
+        listener: (value: T) => void,
+    ): () => void {
+        return subscribeToCoValue(this, id, as, [], listener);
+    }
+
+    /**
+     * Effectful version of `CoMap.subscribe()` that returns a stream of updates.
+     *
+     * Needs to be run inside an `AccountCtx` context.
+     *
+     * @category Subscription & Loading
+     */
+    static subscribeEf<T extends CoPlainText>(
+        this: CoValueClass<T>,
+        id: ID<T>,
+    ): Stream.Stream<T, UnavailableError, AccountCtx> {
+        return subscribeToCoValueEf(this, id, []);
+    }
+
+    /**
+     * Given an already loaded `CoMap`, subscribe to updates to the `CoMap` and ensure that the specified fields are loaded to the specified depth.
+     *
+     * Works like `CoMap.subscribe()`, but you don't need to pass the ID or the account to load as again.
+     *
+     * Returns an unsubscribe function that you should call when you no longer need updates.
+     *
+     * @category Subscription & Loading
+     **/
+    subscribe<T extends CoPlainText>(
+        this: T,
+        listener: (value: T) => void,
+    ): () => void {
+        return subscribeToExistingCoValue(this, [], listener);
+    }
 }
