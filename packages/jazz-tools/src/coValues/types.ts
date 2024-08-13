a2f4130cc2d5ace47ae0f4ffba8bc3d131b6fffe
pax-k <nzpopa@users.noreply.github.com>
2024-08-13 11:11:43 +0300

    fix: lint errors

--- packages/jazz-tools/src/coValues/types.ts
+++ packages/jazz-tools/src/coValues/types.ts
@@ -1,6 +1,6 @@
-import { UnCo } from "../internal";
-import { CoList } from "./coList";
-import { CoMap, CoMapInit } from "./coMap";
+import { UnCo } from "../internal.js";
+import { CoList } from "./coList.js";
+import { CoMap, CoMapInit } from "./coMap.js";
 
 export type RecursiveCoMapInit<T> = T extends CoMap
     ? { [K in keyof CoMapInit<T>]: RecursiveCoMapInit<CoMapInit<T>[K]> }
