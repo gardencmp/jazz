ad416b43c6d6e3e33e34c82f4398fc3e1baf57a3
Callum Flack <callum@callumflack.design>
2024-08-15 18:32:02 +1000

    refactor: fonts

--- website/src/lib/fonts.ts
+++ website/src/lib/fonts.ts
@@ -1,86 +1,80 @@
 import localFont from "next/font/local";
-import { Inter, Manrope } from "next/font/google";
+import { Inter } from "next/font/google";
 
 /* SEE: https://nextjs.org/docs/basic-features/font-optimization */
 
-const manrope = Manrope({
-    subsets: ["latin"],
-    variable: "--font-manrope",
-    display: "swap",
-});
-
 const sans = Inter({
-    subsets: ["latin"],
-    variable: "--font-sans",
-    display: "swap",
+  subsets: ["latin"],
+  variable: "--font-sans",
+  display: "swap",
 });
 
 const display = localFont({
-    variable: "--font-display",
-    src: [
-        {
-            // path: "../public/fonts/SaansTRIAL-Light.woff2",
-            // path: "../public/fonts/Scto-Grotesk-A-Light.woff2",
-            // path: "../public/fonts/Basier-Square-Regular.otf",
-            path: "../public/fonts/Regola-Pro-Regular.otf",
-            weight: "300",
-            style: "normal",
-        },
-        {
-            // path: "../public/fonts/SaansTRIAL-Regular.woff2",
-            // path: "../public/fonts/Scto-Grotesk-A-Regular.woff2",
-            // path: "../public/fonts/Basier-Square-Regular.otf",
-            path: "../public/fonts/Regola-Pro-Regular.otf",
-            weight: "400",
-            style: "normal",
-        },
-        {
-            // path: "../public/fonts/SaansTRIAL-Medium.woff2",
-            // path: "../public/fonts/Scto-Grotesk-A-Medium.woff2",
-            // path: "../public/fonts/Basier-Square-Medium.otf",
-            path: "../public/fonts/Regola-Pro-Medium.otf",
-            weight: "500",
-            style: "normal",
-        },
-        {
-            // path: "../public/fonts/SaansTRIAL-SemiBold.woff2",
-            // path: "../public/fonts/Scto-Grotesk-A-Medium.woff2",
-            // path: "../public/fonts/Basier-Square-SemiBold.otf",
-            path: "../public/fonts/Regola-Pro-Medium.otf",
-            weight: "600",
-            style: "normal",
-        },
-        {
-            // path: "../public/fonts/SaansTRIAL-Bold.woff2",
-            // path: "../public/fonts/Scto-Grotesk-A-Bold.woff2",
-            // path: "../public/fonts/Basier-Square-Bold.otf",
-            path: "../public/fonts/Regola-Pro-Bold.otf",
-            weight: "700",
-            style: "normal",
-        },
-        {
-            // path: "../public/fonts/SaansTRIAL-Heavy.woff2",
-            // path: "../public/fonts/Scto-Grotesk-A-Black.woff2",
-            // path: "../public/fonts/Basier-Square-Bold.otf",
-            path: "../public/fonts/Regola-Pro-Bold.otf",
-            weight: "800",
-            style: "normal",
-        },
-    ],
-    display: "swap",
+  variable: "--font-display",
+  src: [
+    {
+      // path: "../../public/fonts/SaansTRIAL-Light.woff2",
+      // path: "../../public/fonts/Scto-Grotesk-A-Light.woff2",
+      // path: "../../public/fonts/Basier-Square-Regular.otf",
+      path: "../../public/fonts/Regola-Pro-Regular.otf",
+      weight: "300",
+      style: "normal",
+    },
+    {
+      // path: "../../public/fonts/SaansTRIAL-Regular.woff2",
+      // path: "../../public/fonts/Scto-Grotesk-A-Regular.woff2",
+      // path: "../../public/fonts/Basier-Square-Regular.otf",
+      path: "../../public/fonts/Regola-Pro-Regular.otf",
+      weight: "400",
+      style: "normal",
+    },
+    {
+      // path: "../../public/fonts/SaansTRIAL-Medium.woff2",
+      // path: "../../public/fonts/Scto-Grotesk-A-Medium.woff2",
+      // path: "../../public/fonts/Basier-Square-Medium.otf",
+      path: "../../public/fonts/Regola-Pro-Medium.otf",
+      weight: "500",
+      style: "normal",
+    },
+    {
+      // path: "../../public/fonts/SaansTRIAL-SemiBold.woff2",
+      // path: "../../public/fonts/Scto-Grotesk-A-Medium.woff2",
+      // path: "../../public/fonts/Basier-Square-SemiBold.otf",
+      path: "../../public/fonts/Regola-Pro-Medium.otf",
+      weight: "600",
+      style: "normal",
+    },
+    {
+      // path: "../../public/fonts/SaansTRIAL-Bold.woff2",
+      // path: "../../public/fonts/Scto-Grotesk-A-Bold.woff2",
+      // path: "../../public/fonts/Basier-Square-Bold.otf",
+      path: "../../public/fonts/Regola-Pro-Bold.otf",
+      weight: "700",
+      style: "normal",
+    },
+    {
+      // path: "../../public/fonts/SaansTRIAL-Heavy.woff2",
+      // path: "../../public/fonts/Scto-Grotesk-A-Black.woff2",
+      // path: "../../public/fonts/Basier-Square-Bold.otf",
+      path: "../../public/fonts/Regola-Pro-Bold.otf",
+      weight: "800",
+      style: "normal",
+    },
+  ],
+  display: "swap",
 });
 
 const mono = localFont({
-    variable: "--font-mono",
-    src: [
-        {
-            // path: "../public/fonts/ppr_0829.woff2",
-            path: "../public/fonts/SohneMono-Buch.woff2",
-            weight: "400",
-            style: "normal",
-        },
-    ],
-    display: "swap",
+  variable: "--font-mono",
+  src: [
+    {
+      // path: "../../public/fonts/ppr_0829.woff2",
+      path: "../../public/fonts/SohneMono-Buch.woff2",
+      weight: "400",
+      style: "normal",
+    },
+  ],
+  display: "swap",
 });
 
 export { sans, mono, display };
