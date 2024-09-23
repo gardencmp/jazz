46365a35fe105d09fa2982f807a7f8dad228cb92
Trisha Lim <hello@trishalim.com>
2024-09-23 17:38:47 +0100

    Redesign UI of docs side nav

--- homepage/homepage/app/docs/layout.tsx
+++ homepage/homepage/app/docs/layout.tsx
@@ -14,17 +14,13 @@ export default function DocsLayout({
 }) {
     return (
         <div className="relative grid grid-cols-12 gap-x-8 py-8">
-            <div
+            <DocNav
                 className={cn(
                     "md:col-span-4 lg:col-span-3",
                     "sticky align-start top-[4.75rem] h-[calc(100vh-8rem)] overflow-y-auto overflow-x-hidden",
-                    "bg-stone-100 dark:bg-stone-900 text-stone-700 dark:text-stone-300 p-4 rounded-xl",
                     "hidden md:block",
-                    "prose-sm prose-ul:pl-1 prose-ul:ml-1 prose-li:my-2 prose-li:leading-tight prose-ul:list-['-']",
                 )}
-            >
-                <DocNav />
-            </div>
+            />
             <div className="col-span-12 md:col-span-8 lg:col-span-9">
                 <Prose>{children}</Prose>
             </div>
