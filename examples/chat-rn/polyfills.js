140c5fc9286d677d71663a235779110fe33b9cc6
pax-k <nzpopa@users.noreply.github.com>
2024-10-07 15:45:42 +0300

    fix(react-native): polyfils

--- examples/chat-rn/polyfills.js
+++ examples/chat-rn/polyfills.js
@@ -1,5 +1,6 @@
+import "react-native-polyfill-globals/auto";
 import "@azure/core-asynciterator-polyfill";
+import { ReadableStream } from "web-streams-polyfill/ponyfill/es6";
+import { polyfillGlobal } from "react-native/Libraries/Utilities/PolyfillFunctions";
 
-global.TextEncoder = require("text-encoding").TextEncoder;
-global.TextDecoder = require("text-encoding").TextDecoder;
-global.crypto = require("expo-crypto");
+polyfillGlobal("ReadableStream", () => ReadableStream);
