4c9d2b5a4126dd45d127b31d99e482c46044495e
Trisha Lim <hello@trishalim.com>
2024-10-08 11:04:08 +0100

    Limit chat messages to display 20 lines max

--- examples/chat/tailwind.config.js
+++ examples/chat/tailwind.config.js
@@ -69,6 +69,9 @@ module.exports = {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
       },
+      lineClamp: {
+        20: '20',
+      },
     },
   },
   plugins: [require("tailwindcss-animate")],
