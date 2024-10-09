cded520e3fc08cdc4e89385b1d1751159f4cef53
Trisha Lim <hello@trishalim.com>
2024-10-09 20:24:07 +0100

    Add showcase page

---
+++ homepage/homepage/app/showcase/page.tsx
@@ -0,0 +1,44 @@
+import { HeroHeader } from "gcmp-design-system/src/app/components/molecules/HeroHeader";
+import Image from "next/image";
+import { products } from "@/lib/showcase";
+import Link from "next/link";
+
+export const metadata = {
+    title: "jazz - Showcase",
+    description: "A list of cool products built with Jazz.",
+};
+
+export default function Page() {
+    return (
+        <div className="container flex flex-col gap-6 pb-10 lg:pb-20">
+            <HeroHeader
+                title="Cool products built with Jazz"
+                slogan="Find inspiration in what others have made."
+            />
+
+            <div className="grid sm:grid-cols-2 lg:grid-cols-3 gap-8">
+                {products.map((product) => (
+                    <Link
+                        href={product.url}
+                        key={product.url}
+                        className="group border bg-stone-50 shadow-sm p-3 flex flex-col gap-3 rounded-lg md:p-4 md:gap-4 dark:border-stone-900 dark:bg-stone-900"
+                    >
+                        <Image
+                            className="rounded-md border dark:border-0"
+                            src={product.imageUrl}
+                            width="900"
+                            height="675"
+                            alt=""
+                        />
+                        <div className="space-y-2">
+                            <h2 className="font-medium text-stone-900 dark:text-white leading-none">
+                                {product.name}
+                            </h2>
+                            <p className="text-sm">{product.description}</p>
+                        </div>
+                    </Link>
+                ))}
+            </div>
+        </div>
+    );
+}
