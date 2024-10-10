01e9255d3be772790ea81769121775867face43a
Trisha Lim <hello@trishalim.com>
2024-10-10 14:49:59 +0100

    Copy changes
    Co-authored-by: Anselm Eickhoff <anselm.eickhoff@gmail.com>

--- homepage/homepage/lib/showcase.ts
+++ homepage/homepage/lib/showcase.ts
@@ -4,12 +4,12 @@ export const products = [
         imageUrl: "/learn-anything.png",
         url: "https://learn-anything.xyz",
         description:
-            "A free, comprehensive platform that connects you to the best resources for every subject.",
+            "A free, comprehensive learning platform where people collect the best resources for every subject.",
     },
     {
         name: "Invoice Radar",
         imageUrl: "/invoice-radar.png",
         url: "https://invoiceradar.com",
-        description: "Automatically gather invoices from mail and the web.",
+        description: "Automatically gather invoices from mail and cloud providers.",
     },
 ];
