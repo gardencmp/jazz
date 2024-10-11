a9a3ad6e675ca4fcce134485dfed78051e5f929f
Trisha Lim <hello@trishalim.com>
2024-10-11 16:17:12 +0100

    Mobile view

--- homepage/homepage/components/home/Hero.tsx
+++ homepage/homepage/components/home/Hero.tsx
@@ -9,24 +9,24 @@ export function Hero({
     features: { title: string; icon: LucideIcon }[];
 }) {
     return (
-        <div className="container grid grid-cols-2">
-            <div className="pt-24">
-                <h1 className="font-display text-stone-950 dark:text-white text-4xl md:text-5xl lg:text-6xl mb-8 font-medium tracking-tighter">
+        <div className="container grid gap-5 lg:gap-0 md:grid-cols-2">
+            <div className="flex flex-col gap-4 pt-10 md:pt-16 md:pb-16 lg:pb-0 lg:pt-28">
+                <h1 className="font-display text-stone-950 dark:text-white text-4xl md:text-5xl lg:text-6xl font-medium tracking-tighter">
                     Build your next app
                     <br /> with sync
                 </h1>
 
-                <p className="text-lg text-pretty md:leading-relaxed text-stone-700 max-w-2xl dark:text-stone-200 md:text-xl mb-8">
+                <p className="text-pretty md:leading-relaxed text-stone-700 max-w-2xl dark:text-stone-200 md:text-xl">
                     Jazz is an open-source framework for building local-first
                     apps, removing 90% of the backend and infrastructure
                     complexity.
                 </p>
 
-                <div className="grid grid-cols-4 gap-4">
+                <div className="grid grid-cols-2 gap-2 sm:grid-cols-4 md:grid-cols-2 lg:grid-cols-4 sm:gap-4">
                     {features.map(({ title, icon: Icon }) => (
                         <div
                             key={title}
-                            className="flex text-sm gap-2 items-center"
+                            className="flex text-xs sm:text-sm gap-2 items-center"
                         >
                             <span className="text-blue p-1.5 rounded-lg bg-blue-50">
                                 <Icon size={16} />
@@ -37,7 +37,7 @@ export function Hero({
                 </div>
             </div>
 
-            <div className="pl-8 pt-16 -mr-4 flex items-center">
+            <div className="max-w-2xl mx-auto flex items-center sm:pt-16 lg:pl-8 lg:pt-18 md:ml-0 md:-mr-4">
                 <Image
                     src="/code-samples.png"
                     alt="Code samples for defining a schema for Jazz, pushing data, and subscribing to changes."
