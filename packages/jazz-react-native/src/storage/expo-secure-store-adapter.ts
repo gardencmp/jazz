52dd1d37ab341d3bdec72cf382a1b6bddb58b496
pax-k <nzpopa@users.noreply.github.com>
2024-10-14 19:26:05 +0300

    fix(react-native): set ExpoSecureStoreAdapter values accessible AFTER_FIRST_UNLOCK

--- packages/jazz-react-native/src/storage/expo-secure-store-adapter.ts
+++ packages/jazz-react-native/src/storage/expo-secure-store-adapter.ts
@@ -5,18 +5,21 @@ export class ExpoSecureStoreAdapter implements KvStore {
     get(key: string): Promise<string | null> {
         return SecureStore.getItemAsync(key, {
             requireAuthentication: SecureStore.canUseBiometricAuthentication(),
+            keychainAccessible: SecureStore.AFTER_FIRST_UNLOCK,
         });
     }
 
     async set(key: string, value: string): Promise<void> {
         return SecureStore.setItemAsync(key, value, {
             requireAuthentication: SecureStore.canUseBiometricAuthentication(),
+            keychainAccessible: SecureStore.AFTER_FIRST_UNLOCK,
         });
     }
 
     async delete(key: string): Promise<void> {
         return SecureStore.deleteItemAsync(key, {
             requireAuthentication: SecureStore.canUseBiometricAuthentication(),
+            keychainAccessible: SecureStore.AFTER_FIRST_UNLOCK,
         });
     }
 
