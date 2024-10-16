308ab8073652dfe3cc9ccd727fc3f1a359c2ba3b
Trisha Lim <hello@trishalim.com>
2024-10-16 11:27:51 +0100

    Rearrange outline

--- homepage/homepage/lib/docNavigationItems.ts
+++ homepage/homepage/lib/docNavigationItems.ts
@@ -13,58 +13,92 @@ export const docNavigationItems = [
                 name: "Guide",
                 href: "/docs/guide",
             },
-            {
-                // jazz mesh, setting api key, free plan, unlimited
-                name: "Sync and storage",
-                href: "/docs/sync-and-storage",
-            },
+        ],
+    },
+    {
+        name: "Building your app",
+        items: [
             {
                 // installation guide for react, react native
                 name: "Project setup",
                 href: "/docs/project-setup",
             },
-        ],
-    },
-    {
-        // introduction to covalues
-        name: "Schema",
-        href: "/docs/schema",
-        items: [
+            { name: "Authentication", href: "" },
             {
-                name: "Defining",
-                href: "/docs/schema/defining",
+                name: "Schema",
+                href: "",
+                items: [
+                    {
+                        name: "Defining",
+                        href: "/docs/schema/defining",
+                    },
+                    {
+                        name: "Creating",
+                        href: "/docs/schema/creating",
+                    },
+                    {
+                        // loading depth
+                        name: "Reading and subscribing",
+                        href: "/docs/schema/reading",
+                    },
+                    {
+                        name: "Updating",
+                        href: "/docs/schema/updating",
+                    },
+                    {
+                        name: "Deleting",
+                        href: "/docs/schema/deleting",
+                    },
+                ],
             },
+            { name: "Groups & Permissions", href: "" },
             {
-                name: "Creating",
-                href: "/docs/schema/creating",
-            },
-            {
-                // loading depth
-                name: "Reading and subscribing",
-                href: "/docs/schema/reading",
-            },
-            {
-                name: "Updating",
-                href: "/docs/schema/updating",
-            },
-            {
-                name: "Deleting",
-                href: "/docs/schema/deleting",
-            },
-        ],
-    },
-    {
-        // setting up using clerk or demo auth?
-        name: "Authentication",
-        href: "/docs/authentication",
-        items: [
-            {
-                // profile, root, creating initial data
-                name: "Account",
-                href: "/docs/authentication/account",
+                // jazz mesh, setting api key, free plan, unlimited
+                name: "Sync & Storage",
+                href: "/docs/sync-and-storage",
             },
         ],
     },
+    // {
+    //     // introduction to covalues
+    //     name: "Schema",
+    //     href: "/docs/schema",
+    //     items: [
+    //         {
+    //             name: "Defining",
+    //             href: "/docs/schema/defining",
+    //         },
+    //         {
+    //             name: "Creating",
+    //             href: "/docs/schema/creating",
+    //         },
+    //         {
+    //             // loading depth
+    //             name: "Reading and subscribing",
+    //             href: "/docs/schema/reading",
+    //         },
+    //         {
+    //             name: "Updating",
+    //             href: "/docs/schema/updating",
+    //         },
+    //         {
+    //             name: "Deleting",
+    //             href: "/docs/schema/deleting",
+    //         },
+    //     ],
+    // },
+    // {
+    //     // setting up using clerk or demo auth?
+    //     name: "Authentication",
+    //     href: "/docs/authentication",
+    //     items: [
+    //         {
+    //             // profile, root, creating initial data
+    //             name: "Account",
+    //             href: "/docs/authentication/account",
+    //         },
+    //     ],
+    // },
     {
         name: "Resources",
         items: [
