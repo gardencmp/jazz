2c1774dc464334a385ca2adda1daff4552564ed5
Trisha Lim <hello@trishalim.com>
2024-10-17 14:27:49 +0100

    Add content for blog and team pages

--- homepage/gcmp/app/news/page.tsx
+++ homepage/gcmp/app/news/page.tsx
@@ -1,11 +1,29 @@
 import { H1 } from "gcmp-design-system/src/app/components/atoms/Headings";
 import { P } from "gcmp-design-system/src/app/components/atoms/Paragraph";
+import Link from "next/link";
+import {HeroHeader} from "gcmp-design-system/src/app/components/molecules/HeroHeader";
 
 export default function NewsPage() {
     return (
-        <div className="container py-8 lg:py-16">
-            <H1>Blog</H1>
-            <P>Wow! You caught us a bit early.</P>
+        <div className="container">
+            <HeroHeader title="Blog" slogan=""/>
+          <div className="prose">
+
+            <p>Wow! You caught us a bit early.</p>
+            <p>
+                Follow us on <Link href="https://x.com/gcmp_io">@gcmp.io</Link>{" "}
+                or <Link href="https://x.com/jazz_tools">@jazz_tools</Link>.
+            </p>
+
+            <p>
+                And if you want to build something with Jazz, you should
+                definitely{" "}
+                <Link href="https://discord.gg/utDMjHYg42">
+                    join the Jazz Discord
+                </Link>
+                !
+            </p>
+          </div>
         </div>
     );
 }
--- homepage/gcmp/app/team/page.tsx
+++ homepage/gcmp/app/team/page.tsx
@@ -2,11 +2,13 @@ import { H1, H2 } from "gcmp-design-system/src/app/components/atoms/Headings";
 import { MailIcon } from "lucide-react";
 import Link from "next/link";
 import { SiGithub, SiTwitter } from "@icons-pack/react-simple-icons";
+import {SectionHeader} from "gcmp-design-system/src/app/components/molecules/SectionHeader";
+import {HeroHeader} from "gcmp-design-system/src/app/components/molecules/HeroHeader";
 
 export default function TeamPage() {
     return (
-        <div className="container py-8 lg:py-16">
-            <H1>Team</H1>
+        <div className="container">
+            <HeroHeader title="Team" slogan="" />
 
             <div className="grid gap-1">
                 <H2>Anselm Eickhoff (Founder)</H2>
