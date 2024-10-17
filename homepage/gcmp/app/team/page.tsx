2b48954693d408c3cc925239dcdb934481195bc3
Trisha Lim <hello@trishalim.com>
2024-10-17 21:13:18 +0100

    Add team members to team page

--- homepage/gcmp/app/team/page.tsx
+++ homepage/gcmp/app/team/page.tsx
@@ -1,42 +1,123 @@
-import { H1, H2 } from "gcmp-design-system/src/app/components/atoms/Headings";
-import { MailIcon } from "lucide-react";
+import { LucideIcon } from "lucide-react";
 import Link from "next/link";
-import { SiGithub, SiTwitter } from "@icons-pack/react-simple-icons";
-import { SectionHeader } from "gcmp-design-system/src/app/components/molecules/SectionHeader";
+import { SiGithub, SiX } from "@icons-pack/react-simple-icons";
 import { HeroHeader } from "gcmp-design-system/src/app/components/molecules/HeroHeader";
 
-export const metadata = {
-    title: "Team",
-};
+interface TeamMember {
+    name: string;
+    titles: string[];
+    image: string;
+    location: string;
+    x?: string;
+    github?: string;
+}
+
+const team: Array<TeamMember> = [
+    {
+        name: "Anselm Eickhoff",
+        titles: ["Founder"],
+        image: "anselm.jpg",
+        location: "London, UK ",
+        x: "anselm_io",
+        github: "@aeplay",
+    },
+    {
+        name: "Andrei ___",
+        titles: ["Full-Stack Dev", "Infra"],
+        image: "andrei.jpeg",
+        location: "___, Romania ",
+    },
+    {
+        name: "Guido D'Orsi",
+        titles: ["Frontend Dev", "React Performance"],
+        image: "guido.jpeg",
+        location: "Rome, Italy ",
+    },
+    {
+        name: "Trisha Lim",
+        titles: ["Frontend Dev", "Design", "Marketing"],
+        image: "trisha.png",
+        location: "Lisbon, Portugal ",
+    },
+    {
+        name: "Benjamin Leveritt",
+        titles: ["Full-Stack Dev"],
+        image: "benjamin.png",
+        location: "London, UK ",
+    },
+    {
+        name: "Marina ___",
+        titles: ["Full-Stack Dev"],
+        image: "marina.png",
+        location: "___, Spain ",
+    },
+];
+
+function SocialLink({
+    link,
+    label,
+    icon: Icon,
+}: {
+    label: string;
+    link: string;
+    icon: LucideIcon;
+}) {
+    return (
+        <Link href={link} className="p-1 -m-1">
+            <Icon size={16} />
+            <span className="sr-only">{label}</span>
+        </Link>
+    );
+}
+
+export function Person({ person }: { person: TeamMember }) {
+    return (
+        <div className="flex gap-5">
+            <img
+                src={`/team/${person.image}`}
+                className="size-12 rounded-full shadow-sm"
+            />
+            <div className="flex flex-col">
+                <h3 className="text-lg font-semibold leading-8 tracking-tight text-gray-900">
+                    {person.name}
+                </h3>
+                <p className="text-sm leading-6 text-gray-600">
+                    {person.titles.join(", ")}
+                </p>
+                <p className="text-sm leading-6 text-gray-600">
+                    {person.location}
+                </p>
+
+                <div className="flex gap-2 mt-2">
+                    {person.x && (
+                        <SocialLink
+                            link={`https://x.com/${person.x}`}
+                            icon={SiX}
+                            label="X profile"
+                        />
+                    )}
+                    {person.github && (
+                        <SocialLink
+                            link={`https://github.com/${person.github}`}
+                            label="Github profile"
+                            icon={SiGithub}
+                        />
+                    )}
+                </div>
+            </div>
+        </div>
+    );
+}
 
 export default function TeamPage() {
     return (
         <div className="container">
             <HeroHeader title="Team" slogan="" />
 
-            <div className="grid gap-1">
-                <H2>Anselm Eickhoff (Founder)</H2>
-                <Link
-                    href="mailto:anselm@gcmp.io"
-                    className="flex gap-1 items-center"
-                    aria-label="Email address"
-                >
-                    <MailIcon height="1em" /> anselm@gcmp.io
-                </Link>
-                <Link
-                    href="https://x.com/anselm_io"
-                    className="flex gap-1 items-center"
-                    aria-label="Twitter link"
-                >
-                    <SiTwitter height="1em" /> anselm_io
-                </Link>
-                <Link
-                    href="https://github.com/aeplay"
-                    className="flex gap-1 items-center"
-                    aria-label="GitHub link"
-                >
-                    <SiGithub height="1em" /> aeplay
-                </Link>
+            <div className="grid grid-cols-3 gap-10">
+                {team.map((person) => (
+                    <Person key={person.name} person={person} />
+                ))}
             </div>
         </div>
     );
