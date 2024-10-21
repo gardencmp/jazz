35d5e4b39e83003289f1edb79d8d042e70f95186
Trisha Lim <hello@trishalim.com>
2024-10-21 18:07:25 +0100

    Add jazz cloud step

--- homepage/homepage/components/home/HowItWorks.tsx
+++ homepage/homepage/components/home/HowItWorks.tsx
@@ -1,6 +1,7 @@
 import CodeStepOne from "./CodeStepOne.mdx";
 import CodeStepTwo from "./CodeStepTwo.mdx";
 import CodeStepThree from "./CodeStepThree.mdx";
+import CodeStepCloud from "./CodeStepCloud.mdx";
 import { clsx } from "clsx";
 import { H2 } from "gcmp-design-system/src/app/components/atoms/Headings";
 import { GappedGrid } from "gcmp-design-system/src/app/components/molecules/GappedGrid";
@@ -18,15 +19,18 @@ function Code({
         <div
             className={clsx(
                 className,
-                "rounded-t-md flex-1 bg-white border-t border-x border-stone-200 dark:bg-stone-925 dark:border-stone-900",
+                "w-[480px] relative -right-2 max-w-full overflow-x-auto ml-auto rounded-tl-lg overflow-hidden",
+                "shadow-xl shadow-blue/20 ",
+                "border border-stone-200 dark:border-stone-900",
+                "rounded-t-md flex-1 bg-white ring ring-4 ring-stone-400/20 dark:bg-stone-925",
             )}
         >
-            <div className="flex px-3 border-b border-stone-200 dark:border-stone-900">
-                <span className="text-xs border-b border-blue py-1.5">
+            <div className="flex px-4 border-b border-stone-200 dark:border-stone-900">
+                <span className="text-sm border-b border-blue py-2">
                     {fileName}
                 </span>
             </div>
-            <pre className="text-xs">{children}</pre>
+            <pre className="text-sm p-1">{children}</pre>
         </div>
     );
 }
@@ -35,23 +39,29 @@ function Step({
     step,
     description,
     children,
+    className,
 }: {
     step: number;
     description: string;
     children: React.ReactNode;
+    className?: string;
 }) {
     return (
         <div
             className={clsx(
-                "rounded-lg flex flex-col gap-4 pt-4 px-4 bg-white shadow-2xl shadow-stone-800/10 border border-stone-200",
+                className,
+                "rounded-lg overflow-hidden shadow-sm flex flex-col gap-6 bg-white border border-stone-200",
+                "pt-4 lg:pt-6",
                 "dark:bg-stone-925 dark:border-stone-900",
-                "col-span-2",
+                "col-span-2 lg:col-span-3",
             )}
         >
-            <p className="font-handwritten text-stone-900 dark:text-white">
-                Step {step}
-            </p>
-            <h3>{description}</h3>
+            <div className="flex gap-3 px-4 lg:px-6">
+                <p className="font-semibold inline-flex items-center justify-center text-center bg-blue-50 size-8 rounded-full text-blue font-mono">
+                    {step}
+                </p>
+                <p className="max-w-md mt-1.5">{description}</p>
+            </div>
             {children}
         </div>
     );
@@ -84,6 +94,14 @@ export function HowItWorks() {
                 </Step>
                 <Step
                     step={2}
+                    description="Connect to sync and storage infrastructure (Jazz Cloud or self-hosted) using your email address as an API key."
+                >
+                    <Code fileName="main.tsx">
+                        <CodeStepCloud />
+                    </Code>
+                </Step>
+                <Step
+                    step={3}
                     description="Create a Collaborative Value, and it will be synced and persisted automatically."
                 >
                     <Code fileName="sendMessage.ts">
@@ -91,8 +109,8 @@ export function HowItWorks() {
                     </Code>
                 </Step>
                 <Step
-                    step={3}
-                    description="Read your data like simple local state. The UI updates instantly across all devices and users. 🎉"
+                    step={4}
+                    description="Read your data like simple local state. Get instant sync and UI updates across all devices and users. 🎉"
                 >
                     <Code fileName="ChatScreen.tsx">
                         <CodeStepThree />
