1c8049248273d842a2c34b50b7a75d95981a40fb
Trisha Lim <hello@trishalim.com>
2024-10-30 10:51:38 +0000

    Theme toggle button styling

--- homepage/design-system/src/app/components/molecules/ThemeToggle.tsx
+++ homepage/design-system/src/app/components/molecules/ThemeToggle.tsx
@@ -19,7 +19,10 @@ export function ThemeToggle({ className }: { className?: string }) {
             type="button"
             className={clsx(
                 className,
-                "transition-opacity hover:transition-none opacity-60 hover:opacity-100 text-black dark:text-white",
+                "md:p-2 md:rounded-full md:border",
+                "text-stone-400 hover:text-stone-900 dark:text-stone-400 dark:hover:text-white",
+                "md:hover:bg-stone-200 md:dark:hover:bg-stone-900",
+                "transition-colors"
             )}
             aria-label={
                 mounted ? `Switch to ${otherTheme} theme` : "Toggle theme"
@@ -27,12 +30,14 @@ export function ThemeToggle({ className }: { className?: string }) {
             onClick={() => setTheme(otherTheme)}
         >
             <MoonIcon
-                size={20}
-                className="h-5 w-5 stroke-zinc-900 dark:hidden"
+                size={24}
+                strokeWidth={1.5}
+                className="size-5 md:size-6 stroke-stone-900 dark:hidden"
             />
             <SunIcon
-                size={20}
-                className="hidden h-5 w-5 stroke-white dark:block"
+                size={24}
+                strokeWidth={1.5}
+                className="size-5 md:size-6 hidden stroke-white dark:block"
             />
         </button>
     );
