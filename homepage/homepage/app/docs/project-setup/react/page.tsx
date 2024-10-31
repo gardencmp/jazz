9d31bbc3aa548866e527bbd86f56d611b8cd214e
pax-k <ndreipoppa@gmail.com>
2024-10-31 15:20:01 +0200

    chore(docs): added react native docs

--- homepage/homepage/app/docs/project-setup/react/page.tsx
+++ homepage/homepage/app/docs/project-setup/react/page.tsx
@@ -1,4 +1,4 @@
-import ReactGuide from "./react.mdx"
+import ReactGuide from "./react.mdx";
 import { TableOfContents } from "@/components/docs/TableOfContents";
 import { Prose } from "gcmp-design-system/src/app/components/molecules/Prose";
 import { clsx } from "clsx";
@@ -24,21 +24,30 @@ const navItems = [
                 name: "SSR + client-side 🧪",
                 href: "/docs/project-setup/react#next-ssr-plus-csr",
             },
-        ]
+        ],
     },
     {
         name: "React Native",
-        href: "/docs/project-setup/react#react-native"
-    }
-]
-
+        href: "/docs/project-setup/react#react-native",
+        items: [
+            {
+                name: "Setup",
+                href: "/docs/project-setup/react#react-native-setup",
+            },
+            {
+                name: "Using Jazz",
+                href: "/docs/project-setup/react#react-native-using-jazz",
+            },
+        ],
+    },
+];
 
 export default function Page() {
     return (
         <div
             className={clsx(
                 "col-span-12 md:col-span-8 lg:col-span-9",
-                "lg:flex lg:gap-5",
+                "flex justify-center lg:gap-5",
             )}
         >
             <Prose className="overflow-x-hidden lg:flex-1">
