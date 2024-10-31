9eef1ec03179a1c1fc65fc05d6a79c5942216321
pax-k <ndreipoppa@gmail.com>
2024-10-31 15:33:54 +0200

    fix(docs): layout centering

--- homepage/homepage/app/docs/guide/page.tsx
+++ homepage/homepage/app/docs/guide/page.tsx
@@ -77,7 +77,7 @@ export default function Page() {
         <div
             className={clsx(
                 "col-span-12 md:col-span-8 lg:col-span-9",
-                "lg:flex lg:gap-5",
+                "flex justify-center lg:gap-5",
             )}
         >
             <Prose className="overflow-x-hidden lg:flex-1">
