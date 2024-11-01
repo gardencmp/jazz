a62f0bf85282db9c80f5cdf03112cd6eba520f71
Trisha Lim <hello@trishalim.com>
2024-11-01 13:05:38 +0000

    Illustration for "file uploads" feature

--- homepage/homepage/components/home/FeaturesSection.tsx
+++ homepage/homepage/components/home/FeaturesSection.tsx
@@ -1,13 +1,30 @@
 import { H3 } from "gcmp-design-system/src/app/components/atoms/Headings";
 import { Prose } from "gcmp-design-system/src/app/components/molecules/Prose";
 import { GappedGrid } from "gcmp-design-system/src/app/components/molecules/GappedGrid";
-import { CheckIcon, ImageIcon, UploadCloudIcon, UserIcon } from "lucide-react";
+import {
+    CheckIcon,
+    FileTextIcon,
+    ImageIcon,
+    TrashIcon,
+    UploadCloudIcon,
+    UserIcon,
+} from "lucide-react";
 import { LabelledFeatureIcon } from "gcmp-design-system/src/app/components/molecules/LabelledFeatureIcon";
 import { Button } from "gcmp-design-system/src/app/components/atoms/Button";
 import { ComingSoonBadge } from "gcmp-design-system/src/app/components/atoms/ComingSoonBadge";
 import { clsx } from "clsx";
 import { SectionHeader } from "gcmp-design-system/src/app/components/molecules/SectionHeader";
 import Link from "next/link";
+
+function FileUploadsIllustration() {
+    return (
+        <div>
+            <pre>
+                <code>BinaryCoStream.createFromBlob(file, ownership);</code>
+            </pre>
+        </div>
+    );
+}
 
 export function FeaturesSection() {
     const features = [
@@ -23,6 +40,33 @@ export function FeaturesSection() {
                     </p>
                 </>
             ),
+            illustration: (
+                <div className="flex w-full h-full flex-col pt-4 gap-4 items-center justify-center bg-gradient-to-b from-stone-50 to-transparent">
+                    <div className="grid gap-4 mx-auto">
+                        <pre className="">
+                            <code className="text-stone-900 dark:text-white">
+                                BinaryCoStream.createFromBlob(file);
+                            </code>
+                        </pre>
+
+                        <div className="w-full bg-white rounded-md py-3 px-3 flex gap-4 items-center border rounded-xl shadow-lg shadow-stone-500/10">
+                            <FileTextIcon
+                                size={32}
+                                strokeWidth={1}
+                                className="text-blue dark:text-white"
+                            />
+                            <div className="text-2xl flex-1 text-blue dark:text-white">
+                                file.pdf
+                            </div>
+                            <TrashIcon
+                                size={32}
+                                strokeWidth={1}
+                                className="text-stone-500"
+                            />
+                        </div>
+                    </div>
+                </div>
+            ),
         },
         {
             title: "Progressive image loading",
@@ -34,6 +78,7 @@ export function FeaturesSection() {
                     and image size info.
                 </>
             ),
+            illustration: <></>,
         },
         {
             title: "State management",
@@ -44,15 +89,21 @@ export function FeaturesSection() {
                     built-in.
                 </>
             ),
+            illustration: <></>,
         },
         {
             title: "Authentication",
             icon: UserIcon,
             description: (
                 <>
-                    <p>Plug and play different kinds of auth like WebAuthN (Touch ID, Face ID), and Clerk. Auth0, Okta, NextAuth coming soon.</p>
+                    <p>
+                        Plug and play different kinds of auth like WebAuthN
+                        (Touch ID, Face ID), and Clerk. Auth0, Okta, NextAuth
+                        coming soon.
+                    </p>
                 </>
             ),
+            illustration: <></>,
         },
     ];
 
@@ -72,15 +123,24 @@ export function FeaturesSection() {
             />
 
             <GappedGrid>
-                {features.map(({ title, icon: Icon, description }) => (
-                  <div key={title} className="col-span-2 border rounded-xl shadow-sm overflow-hidden">
-                      <div className="bg-gray-100 w-full h-48"></div>
-                      <div className="p-4">
-                          <h3 className="font-medium text-stone-900 dark:text-white mb-1">{title}</h3>
-                          <Prose size="sm">{description}</Prose>
-                      </div>
-                  </div>
-                ))}
+                {features.map(
+                    ({ title, icon: Icon, description, illustration }) => (
+                        <div
+                            key={title}
+                            className="col-span-2 border rounded-xl shadow-sm overflow-hidden"
+                        >
+                            <div className="w-full h-48 flex items-center">
+                                {illustration}
+                            </div>
+                            <div className="p-4">
+                                <h3 className="font-medium text-stone-900 dark:text-white mb-1">
+                                    {title}
+                                </h3>
+                                <Prose size="sm">{description}</Prose>
+                            </div>
+                        </div>
+                    ),
+                )}
                 {/*<LabelledFeatureIcon*/}
                 {/*  className="col-span-2"*/}
                 {/*  key={title}*/}
@@ -90,7 +150,7 @@ export function FeaturesSection() {
                 {/*/>*/}
 
                 <div className="border p-4 sm:p-8 bg-gradient-to-t from-blue-50/50 via-30% via-transparent to-transparent shadow-sm rounded-xl col-span-2 md:col-span-4 space-y-5">
-                    <H3 className="!text-blue">Jazz Cloud</H3>
+                    <H3>Jazz Cloud</H3>
                     <Prose className="max-w-xl">
                         <p>
                             Jazz Cloud is a real-time sync and storage
@@ -123,13 +183,13 @@ export function FeaturesSection() {
                             View full pricing
                         </Button>
                     </div>
-                        <Prose size="sm">
-                            You can rely on us, or{" "}
-                            <Link href="/docs/sync-and-storage#running-your-own">
-                                self-host
-                            </Link>
-                            .
-                        </Prose>
+                    <Prose size="sm">
+                        You can rely on us, or{" "}
+                        <Link href="/docs/sync-and-storage#running-your-own">
+                            self-host
+                        </Link>
+                        .
+                    </Prose>
                 </div>
             </GappedGrid>
         </div>
