ec91b6b3c24f6cc28dccb3725aa5b51c059d9197
Trisha Lim <hello@trishalim.com>
2024-11-01 18:40:48 +0000

    Add "offline-first" feature and redesign layout for local-first section

--- homepage/homepage/components/home/LocalFirstFeaturesSection.tsx
+++ homepage/homepage/components/home/LocalFirstFeaturesSection.tsx
@@ -4,20 +4,30 @@ import {
     GaugeIcon,
     MonitorSmartphoneIcon,
     MousePointerSquareDashedIcon,
+    WifiOffIcon,
 } from "lucide-react";
 import { LabelledFeatureIcon } from "gcmp-design-system/src/app/components/molecules/LabelledFeatureIcon";
+import { Prose } from "gcmp-design-system/src/app/components/molecules/Prose";
 
 export function LocalFirstFeaturesSection() {
     const features = [
+        {
+            title: "Offline-first",
+            icon: WifiOffIcon,
+            description: (
+                <>
+                    Your app works seamlessly offline or on sketchy connections.
+                    When you&apos;re online, your data is synced.
+                </>
+            ),
+        },
         {
             title: "Instant updates",
             icon: GaugeIcon,
             description: (
                 <>
-                    <p>
-                        Since you&apos;re working with local state, your UI
-                        updates instantly. No spinners and API calls.
-                    </p>
+                    Since you&apos;re working with local state, your UI updates
+                    instantly. No spinners and API calls.
                 </>
             ),
         },
@@ -26,10 +36,8 @@ export function LocalFirstFeaturesSection() {
             icon: MonitorSmartphoneIcon,
             description: (
                 <>
-                    <p>
-                        Every device with the same account will always have
-                        everything in sync.
-                    </p>
+                    Every device with the same account will always have
+                    everything in sync.
                 </>
             ),
         },
@@ -38,45 +46,59 @@ export function LocalFirstFeaturesSection() {
             icon: MousePointerSquareDashedIcon,
             description: (
                 <>
-                    <p>
-                        Share state with other users, and get automatic
-                        real-time multiplayer.
-                    </p>
+                    Adding multiplayer is as easy as sharing state with other
+                    users.
                 </>
             ),
         },
     ];
     return (
-        <div>
-            <SectionHeader
-                title="Why local-first?"
-                slogan={
-                    <>
-                        <p>
-                            Local-first apps primarily store data locally, so
-                            your users can work offline. When they&apos;re back
-                            online, their local changes are synced to the
-                            server.
-                        </p>
-                        <p>
-                            It turns the data flow around, giving you mutable
-                            local state, instantly synced.
-                        </p>
-                    </>
-                }
-            />
+        <GappedGrid>
+            <div className="col-span-3">
+                <SectionHeader
+                    title="Why local-first?"
+                    slogan={
+                        <>
+                            <p>
+                                With local-first, your data is stored locally,
+                                then synced to the server. This comes with the
+                                following benefits.
+                            </p>
+                        </>
+                    }
+                />
 
-            <GappedGrid>
-                {features.map(({ title, icon: Icon, description }) => (
-                    <LabelledFeatureIcon
-                        className="col-span-2"
-                        key={title}
-                        label={title}
-                        icon={Icon}
-                        explanation={description}
-                    />
-                ))}
-            </GappedGrid>
-        </div>
+                <div className="mt-8 flex flex-col gap-6">
+                    {features.map(({ title, icon: Icon, description }) => (
+                        <div key={title} className="flex items-center gap-4">
+                            <Icon
+                                size={36}
+                                className="shrink-0  size-12 text-blue p-2 rounded-lg bg-blue-50 dark:text-blue-500 dark:bg-stone-900"
+                                strokeWidth={1.5}
+                            />
+                            <Prose>
+                                <p>
+                                    <strong>{title}</strong>. {description}
+                                </p>
+                            </Prose>
+                        </div>
+                    ))}
+                </div>
+            </div>
+
+            <div className="h-full bg-stone-200 col-span-3" />
+
+            {/*<GappedGrid>*/}
+            {/*    {features.map(({ title, icon: Icon, description }) => (*/}
+            {/*        <LabelledFeatureIcon*/}
+            {/*            className="col-span-2"*/}
+            {/*            key={title}*/}
+            {/*            label={title}*/}
+            {/*            icon={Icon}*/}
+            {/*            explanation={description}*/}
+            {/*        />*/}
+            {/*    ))}*/}
+            {/*</GappedGrid>*/}
+        </GappedGrid>
     );
 }
