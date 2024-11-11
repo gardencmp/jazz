b529ae3c550a9ce6e78d9131dbd5d7e1aa727625
Guido D'Orsi <gu.dorsi@gmail.com>
2024-11-11 22:46:56 +0100

    fix: hack a solution for the chat rooms sync

--- homepage/homepage/components/ResponsiveIframe.tsx
+++ homepage/homepage/components/ResponsiveIframe.tsx
@@ -1,6 +1,12 @@
 "use client";
 
-import { IframeHTMLAttributes, useLayoutEffect, useRef, useState } from "react";
+import {
+  IframeHTMLAttributes,
+  useLayoutEffect,
+  useMemo,
+  useRef,
+  useState,
+} from "react";
 
 const dimensions = {
   width: 200,
@@ -23,8 +29,7 @@ function Iframe(props: IframeHTMLAttributes<HTMLIFrameElement>) {
 export function ResponsiveIframe(
   props: IframeHTMLAttributes<HTMLIFrameElement> & { localsrc: string },
 ) {
-  const [url, setUrl] = useState<string | undefined>();
-  const [src, setSrc] = useState<string | undefined>();
+  const [chatId, setChatId] = useState<string | undefined>();
 
   const user1 = "A";
   const user2 = "B";
@@ -32,36 +37,53 @@ export function ResponsiveIframe(
   const isLocal = window.location.hostname === "localhost";
   // const isLocal = false;
 
-  const [src1, setSrc1] = useState(
-    (isLocal ? "http://localhost:5173" : "https://chat.jazz.tools") +
-      `?user=${user1}`,
-  );
-  const [src2, setSrc2] = useState(
-    (isLocal ? "http://localhost:5174" : "https://jazz-chat-2.vercel.app") +
-      `?user=${user2}`,
-  );
+  const server1 = isLocal
+    ? "http://localhost:5173"
+    : "https://chat.jazz.tools" + `?user=${user1}`;
+
+  const server2 = isLocal
+    ? "http://localhost:5174"
+    : "https://jazz-chat-2.vercel.app" + `?user=${user2}`;
 
   useLayoutEffect(() => {
+    if (chatId) return; // Once the chatId is set, we don't need to listen for messages anymore
+
+    const server1Url = new URL(server1);
+    const server2Url = new URL(server2);
+
     const listener = (e: MessageEvent) => {
-      console.log("navigate", e.data.url);
-      if (e.data.type === "navigate" && src?.startsWith(e.origin)) {
-        setUrl(e.data.url);
+      const isValidOrigin =
+        e.origin === server1Url.origin || e.origin === server2Url.origin;
+
+      if (e.data.type === "chat-load" && isValidOrigin && e.data.id) {
+        setChatId(e.data.id);
       }
     };
     window.addEventListener("message", listener);
     return () => {
       window.removeEventListener("message", listener);
     };
-  }, [src]);
+  }, [chatId]);
 
-  useLayoutEffect(() => {
-    setSrc(
-      window.location.hostname === "localhost" ? props.localsrc : props.src,
-    );
-    setUrl(
-      window.location.hostname === "localhost" ? props.localsrc : props.src,
-    );
-  }, [props.src, props.localsrc]);
+  const src1 = useMemo(() => {
+    if (chatId) {
+      const server1Url = new URL(server1);
+      server1Url.hash = chatId;
+      return server1Url.toString();
+    }
+
+    return server1;
+  }, [chatId, server1]);
+
+  const src2 = useMemo(() => {
+    if (chatId) {
+      const server2Url = new URL(server2);
+      server2Url.hash = chatId;
+      return server2Url.toString();
+    }
+
+    return server2;
+  }, [chatId, server2]);
 
   return (
     <div className="grid grid-cols-2 justify-center gap-8">
