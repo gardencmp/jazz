01f404c72b890c937305c00baab5193b7032b2ae
Guido D'Orsi <gu.dorsi@gmail.com>
2024-11-12 22:48:03 +0100

    remove chat-vue dist files

--- packages/jazz-vue/dist/auth/DemoAuthBasicUI.vue.d.ts
+++
@@ -1,9 +0,0 @@
-import { DemoAuthState } from './useDemoAuth.js';
-declare const _default: import('vue').DefineComponent<{
-    appName: string;
-    state: DemoAuthState;
-}, {}, {}, {}, {}, import('vue').ComponentOptionsMixin, import('vue').ComponentOptionsMixin, {}, string, import('vue').PublicProps, Readonly<{
-    appName: string;
-    state: DemoAuthState;
-}> & Readonly<{}>, {}, {}, {}, {}, string, import('vue').ComponentProvideOptions, false, {}, any>;
-export default _default;
--- packages/jazz-vue/dist/index.d.ts
+++
@@ -1,4 +0,0 @@
-export * from './createJazzVueApp.js';
-export * from './auth/useDemoAuth.js';
-export { default as DemoAuthBasicUI } from './auth/DemoAuthBasicUI.vue';
-export { default as ProgressiveImg } from './ProgressiveImg.vue';
