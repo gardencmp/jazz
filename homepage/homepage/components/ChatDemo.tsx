0c781d28f7264ca99400e7ac3c641d60b02828c6
Trisha Lim <hello@trishalim.com>
2024-11-12 12:03:03 +0000

    fix window is undefined

--- homepage/homepage/components/ChatDemo.tsx
+++ homepage/homepage/components/ChatDemo.tsx
@@ -2,6 +2,7 @@
 
 import {
   IframeHTMLAttributes,
+  useEffect,
   useLayoutEffect,
   useMemo,
   useRef,
@@ -32,19 +33,26 @@ export function ChatDemo() {
   const user1 = "A";
   const user2 = "B";
 
-  const isLocal = window.location.hostname === "localhost";
-  // const isLocal = false;
-
-  const server1 = isLocal
-    ? "http://localhost:5173"
-    : "https://chat.jazz.tools" + `?user=${user1}`;
-
-  const server2 = isLocal
-    ? "http://localhost:5174"
-    : "https://jazz-chat-2.vercel.app" + `?user=${user2}`;
+  const [server1, setServer1] = useState<string | null>();
+  const [server2, setServer2] = useState<string | null>();
 
   useLayoutEffect(() => {
     if (chatId) return; // Once the chatId is set, we don't need to listen for messages anymore
+
+    if (typeof window === "undefined") return;
+
+    const isLocal = window.location.hostname === "localhost";
+
+    setServer1(
+      (isLocal ? "http://localhost:5173" : "https://chat.jazz.tools") +
+        `?user=${user1}`,
+    );
+    setServer2(
+      (isLocal ? "http://localhost:5174" : "https://jazz-chat-2.vercel.app") +
+        `?user=${user2}`,
+    );
+
+    if (!server1 || !server2) return;
 
     const server1Url = new URL(server1);
     const server2Url = new URL(server2);
@@ -61,10 +69,10 @@ export function ChatDemo() {
     return () => {
       window.removeEventListener("message", listener);
     };
-  }, [chatId]);
+  }, [chatId, server1, server2]);
 
   const src1 = useMemo(() => {
-    if (chatId) {
+    if (chatId && server1) {
       const server1Url = new URL(server1);
       server1Url.hash = chatId;
       return server1Url.toString();
@@ -74,7 +82,7 @@ export function ChatDemo() {
   }, [chatId, server1]);
 
   const src2 = useMemo(() => {
-    if (chatId) {
+    if (chatId && server2) {
       const server2Url = new URL(server2);
       server2Url.hash = chatId;
       return server2Url.toString();
@@ -82,6 +90,8 @@ export function ChatDemo() {
 
     return server2;
   }, [chatId, server2]);
+
+  if (!src1 || !src2) return null;
 
   return (
     <div className="grid grid-cols-2 justify-center gap-8">
