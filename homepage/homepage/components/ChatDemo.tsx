a64690e5f350d0bbc61fa5cec25e98580db95f5e
Trisha Lim <hello@trishalim.com>
2024-11-15 18:30:57 +0000

    dont hide logout

--- homepage/homepage/components/ChatDemo.tsx
+++ homepage/homepage/components/ChatDemo.tsx
@@ -16,12 +16,9 @@ const dimensions = {
 function Iframe(
   props: IframeHTMLAttributes<HTMLIFrameElement> & {
     user: string;
-    avatarClassName: string;
   },
 ) {
-  const { src, user, avatarClassName } = props;
-
-  const initials = user[0];
+  const { src, user } = props;
 
   return (
     <div className="relative col-span-2 w-full border rounded-xl shadow-sm overflow-hidden lg:col-span-2 dark:bg-black">
@@ -32,17 +29,6 @@ function Iframe(
         {...dimensions}
         allowFullScreen
       />
-      <div className="absolute top-0 left-0 w-full flex items-center gap-2 p-3 bg-white w-full border-b dark:bg-transparent">
-        <div
-          className={clsx(
-            "size-8 lead rounded-full text-white font-medium inline-flex items-center justify-center",
-            avatarClassName,
-          )}
-        >
-          {initials}
-        </div>
-        {user}
-      </div>
     </div>
   );
 }
@@ -103,13 +89,9 @@ export function ChatDemo() {
 
   return (
     <>
-      <Iframe src={server1} user={user1} avatarClassName="bg-pink-600" />
+      <Iframe src={server1} user={user1} />
       {server2WithSameChatId && (
-        <Iframe
-          src={server2WithSameChatId}
-          user={user2}
-          avatarClassName="bg-green-600"
-        />
+        <Iframe src={server2WithSameChatId} user={user2} />
       )}
     </>
   );
