2dfe2158fd1d773db045a9be8c9142390043c15b
Anselm Eickhoff <anselm.eickhoff@gmail.com>
2024-11-18 15:12:11 +0000

    Merge pull request #790 from gardencmp/trishalim-jazz-474
    Fix: page reloads when clicking a link inside mdx

--- homepage/homepage/mdx-components.tsx
+++ homepage/homepage/mdx-components.tsx
@@ -1,7 +1,14 @@
 import type { MDXComponents } from "mdx/types";
+import Link from "next/link";
 
 export function useMDXComponents(components: MDXComponents): MDXComponents {
   return {
+    a: (props) =>
+      props.href ? (
+        <Link href={props.href}>{props.children}</Link>
+      ) : (
+        props.children
+      ),
     ...components,
   };
 }
