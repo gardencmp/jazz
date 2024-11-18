877596f3891cc9694f865691b0c36df3426fbfbc
pax-k <ndreipoppa@gmail.com>
2024-11-18 18:44:34 +0200

    chore: added typedoc for SchemaUnion

--- packages/jazz-tools/src/coValues/schemaUnion.ts
+++ packages/jazz-tools/src/coValues/schemaUnion.ts
@@ -5,11 +5,77 @@ import {
   CoValueFromRaw,
 } from "../internal.js";
 
+/**
+ * SchemaUnion allows you to create union types of CoValues that can be discriminated at runtime.
+ *
+ * @categoryDescription Declaration
+ * Declare your union types by extending `SchemaUnion.Of(...)` and passing a discriminator function
+ * that determines which concrete type to use based on the raw data.
+ *
+ * ```ts
+ * import { SchemaUnion, CoMap } from "jazz-tools";
+ *
+ * class BaseWidget extends CoMap {
+ *   type = co.string;
+ * }
+ *
+ * class ButtonWidget extends BaseWidget {
+ *   type = co.literal("button");
+ *   label = co.string;
+ * }
+ *
+ * class SliderWidget extends BaseWidget {
+ *   type = co.literal("slider");
+ *   min = co.number;
+ *   max = co.number;
+ * }
+ *
+ * const WidgetUnion = SchemaUnion.Of<BaseWidget>((raw) => {
+ *   switch (raw.get("type")) {
+ *     case "button": return ButtonWidget;
+ *     case "slider": return SliderWidget;
+ *     default: throw new Error("Unknown widget type");
+ *   }
+ * });
+ * ```
+ *
+ * @categoryDescription Loading
+ * When loading a SchemaUnion, the correct subclass will be instantiated based on the discriminator.
+ *
+ * ```ts
+ * const widget = await WidgetUnion.load(id, me, {});
+ * if (widget instanceof ButtonWidget) {
+ *   console.log(widget.label);
+ * } else if (widget instanceof SliderWidget) {
+ *   console.log(widget.min, widget.max);
+ * }
+ * ```
+ *
+ * @category CoValues
+ */
 export abstract class SchemaUnion extends CoValueBase implements CoValue {
-  static fromRaw<V extends CoValue>(this: CoValueClass<V>, raw: V["_raw"]): V {
-    throw new Error("Not implemented");
-  }
-
+  /**
+   * Create a new union type from a discriminator function.
+   *
+   * The discriminator function receives the raw data and should return the appropriate
+   * concrete class to use for that data.
+   *
+   * @param discriminator - Function that determines which concrete type to use
+   * @returns A new class that can create/load instances of the union type
+   *
+   * @example
+   * ```ts
+   * const WidgetUnion = SchemaUnion.Of<BaseWidget>((raw) => {
+   *   switch (raw.get("type")) {
+   *     case "button": return ButtonWidget;
+   *     case "slider": return SliderWidget;
+   *     default: throw new Error("Unknown widget type");
+   *   }
+   * });
+   * ```
+   *
+   * @category Creation
+   */
   static Of<V extends CoValue>(
     discriminator: (raw: V["_raw"]) => CoValueClass<V> & CoValueFromRaw<V>,
   ): CoValueClass<V> & typeof SchemaUnion {
@@ -24,5 +90,15 @@ export abstract class SchemaUnion extends CoValueBase implements CoValue {
         return ResolvedClass.fromRaw(raw);
       }
     } as unknown as CoValueClass<V> & typeof SchemaUnion;
+  }
+
+  /**
+   * Create an instance from raw data. This is called internally and should not be used directly.
+   * Use {@link SchemaUnion.Of} to create a union type instead.
+   *
+   * @internal
+   */
+  static fromRaw<V extends CoValue>(this: CoValueClass<V>, raw: V["_raw"]): V {
+    throw new Error("Not implemented");
   }
 }
