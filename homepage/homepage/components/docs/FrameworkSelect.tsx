5e051020ea1774d8b9402c8ab2494f6c82305412
Trisha Lim <hello@trishalim.com>
2024-11-20 14:12:43 +0000

    Set url param for framework

--- homepage/homepage/components/docs/FrameworkSelect.tsx
+++ homepage/homepage/components/docs/FrameworkSelect.tsx
@@ -1,9 +1,38 @@
+"use client";
+
 import { clsx } from "clsx";
 import { Select } from "gcmp-design-system/src/app/components/molecules/Select";
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
+import { useState } from "react";
 
 export function FrameworkSelect({ className }: { className?: string }) {
+  const searchParams = useSearchParams();
+  const router = useRouter();
+  const [framework, setFramework] = useState(
+    searchParams.get("framework") || "react",
+  );
+
+  const path = usePathname();
+
+  const onChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    e.preventDefault();
+
+    const newParams = new URLSearchParams({ framework: e.target.value });
+    const newFramework = e.target.value;
+
+    setFramework(newFramework);
+
+    newParams.set("framework", newFramework);
+    router.push(`${path}?${newParams.toString()}`);
+  };
+
   return (
-    <Select label="Framework" className={clsx("label:sr-only", className)}>
+    <Select
+      label="Framework"
+      value={framework}
+      onChange={onChange}
+      className={clsx("label:sr-only", className)}
+    >
       <option value="react">React</option>
       <option value="react-native">React Native</option>
       <option value="vue">Vue</option>
