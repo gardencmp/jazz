0c95d4af0bc26babe4c7117fbf56a488af29ff5e
Benjamin S. Leveritt <benjamin@leveritt.co.uk>
2024-11-20 09:17:20 +0000

    Add clerk integration

--- examples/minimal-auth-clerk/src/main.tsx
+++ examples/minimal-auth-clerk/src/main.tsx
@@ -1,10 +1,50 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { ClerkProvider, SignInButton, useClerk } from "@clerk/clerk-react";
+import { createJazzReactApp } from "jazz-react";
+import { useJazzClerkAuth } from "jazz-react-auth-clerk";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
 
-createRoot(document.getElementById('root')!).render(
+// Import your publishable key
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
+
+if (!PUBLISHABLE_KEY) {
+  throw new Error("Add your Clerk publishable key to the .env.local file");
+}
+
+const Jazz = createJazzReactApp();
+export const { useAccount, useCoState } = Jazz;
+
+function JazzAndAuth({ children }: { children: React.ReactNode }) {
+  const clerk = useClerk();
+  const [auth, state] = useJazzClerkAuth(clerk);
+
+  return (
+    <>
+      {state?.errors?.map((error) => (
+        <div key={error}>{error}</div>
+      ))}
+      {clerk.user && auth ? (
+        <Jazz.Provider
+          auth={auth}
+          peer="wss://cloud.jazz.tools/?key=minimal-auth-clerk-example@gcmp.io"
+        >
+          {children}
+        </Jazz.Provider>
+      ) : (
+        <SignInButton />
+      )}
+    </>
+  );
+}
+
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <ClerkProvider publishableKey={PUBLISHABLE_KEY} afterSignOutUrl="/">
+      <JazzAndAuth>
+        <App />
+      </JazzAndAuth>
+    </ClerkProvider>
   </StrictMode>,
-)
+);
