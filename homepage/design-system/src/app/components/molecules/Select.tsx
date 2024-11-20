65b7930b1beae7f7b22a325e820b07beccad0d4f
Trisha Lim <hello@trishalim.com>
2024-11-20 13:40:41 +0000

    Add framework selector on side nav

---
+++ homepage/design-system/src/app/components/molecules/Select.tsx
@@ -0,0 +1,40 @@
+import { clsx } from "clsx";
+import { ChevronDownIcon } from "lucide-react";
+import { useId } from "react";
+
+export function Select(
+  props: React.SelectHTMLAttributes<HTMLSelectElement> & { label: string },
+) {
+  const { label, id: customId, className, size } = props;
+  const generatedId = useId();
+  const id = customId || generatedId;
+
+  const containerClassName = clsx("grid gap-1", className);
+
+  const selectClassName = clsx(
+    "g-select",
+    "w-full rounded-md border shadow-sm px-2 py-1.5 text-sm",
+    "font-medium text-stone-900",
+    "dark:text-white",
+    "appearance-none",
+  );
+
+  return (
+    <div className={containerClassName}>
+      <label htmlFor={id} className="text-stone-600 dark:text-stone-300">
+        {label}
+      </label>
+
+      <div className="relative flex items-center">
+        <select id={id} className={selectClassName}>
+          {props.children}
+        </select>
+
+        <ChevronDownIcon
+          className="absolute right-[0.5em] text-stone-400 dark:text-stone-600"
+          size={16}
+        />
+      </div>
+    </div>
+  );
+}
