304d4ca5ce52f2406208001376de865fa9cf02c7
Trisha Lim <hello@trishalim.com>
2024-11-21 17:21:35 +0000

    Store selected framework in FrameworkContext

--- homepage/homepage/app/docs/[framework]/layout.tsx
+++ homepage/homepage/app/docs/[framework]/layout.tsx
@@ -1,3 +1,4 @@
+import { Providers } from "@/components/Providers";
 import DocsLayout from "@/components/docs/DocsLayout";
 import { FrameworkSelect } from "@/components/docs/FrameworkSelect";
 import { DocNav } from "@/components/docs/nav";
@@ -15,13 +16,13 @@ export default function Layout({
   params: { framework: string };
 }) {
   return (
-    <DocsLayout
-      nav={<DocNav framework={params.framework} />}
-      navHeader={
-        <FrameworkSelect className="mb-8" framework={params.framework} />
-      }
-    >
-      <div className="flex justify-center lg:gap-5">{children}</div>
-    </DocsLayout>
+    <Providers framework={params.framework}>
+      <DocsLayout
+        nav={<DocNav framework={params.framework} />}
+        navHeader={<FrameworkSelect className="mb-8" />}
+      >
+        <div className="flex justify-center lg:gap-5">{children}</div>
+      </DocsLayout>
+    </Providers>
   );
 }
