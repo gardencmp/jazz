d55add77729cfcc266deb4cf93b85ce5be78651d
Benjamin S. Leveritt <benjamin@leveritt.co.uk>
2024-11-25 22:16:09 +0000

    Fix exports for rich-text example

--- packages/jazz-tools/src/exports.ts
+++ packages/jazz-tools/src/exports.ts
@@ -17,17 +17,17 @@ export {
   isControlledAccount,
   type AccountClass,
 } from "./coValues/account.js";
-export { Group } from "./coValues/group.js";
 export {
-  CoStream,
-  CoFeed,
-  FileStream,
   BinaryCoStream,
+  CoFeed,
+  CoStream,
+  FileStream,
 } from "./coValues/coFeed.js";
 export { CoList } from "./coValues/coList.js";
 export { CoMap, type CoMapInit } from "./coValues/coMap.js";
-export { CoValueBase } from "./coValues/interfaces.js";
 export { ImageDefinition } from "./coValues/extensions/imageDef.js";
+export { Group } from "./coValues/group.js";
+export { CoValueBase } from "./coValues/interfaces.js";
 export { Profile } from "./coValues/profile.js";
 export { SchemaUnion } from "./coValues/schemaUnion.js";
 
