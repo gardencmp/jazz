2870ec377da3992ee59933de4f37def58f956413
Trisha Lim <hello@trishalim.com>
2024-11-25 10:53:08 +0000

    Reuse FeatureCard on Coming Soon section

--- homepage/design-system/src/app/components/molecules/FeatureCard.tsx
+++ homepage/design-system/src/app/components/molecules/FeatureCard.tsx
@@ -7,25 +7,30 @@ export function FeatureCard({
   label,
   icon: Icon,
   explanation,
+  children,
   className,
 }: {
-  label: string;
-  icon: LucideIcon;
-  explanation: React.ReactNode;
+  label: React.ReactNode;
+  icon?: LucideIcon;
+  explanation?: React.ReactNode;
+  children?: React.ReactNode;
   className?: string;
 }) {
   return (
     <Card className={clsx(className, "p-4")}>
-      <Icon
-        className="size-8 text-blue p-1.5 rounded-lg bg-blue-50 dark:text-blue-500 dark:bg-stone-900 mb-2.5 md:size-10"
-        strokeWidth={1.5}
-        strokeLinecap="butt"
-        size={80}
-      />
+      {Icon && (
+        <Icon
+          className="size-8 text-blue p-1.5 rounded-lg bg-blue-50 dark:text-blue-500 dark:bg-stone-900 mb-2.5 md:size-10"
+          strokeWidth={1.5}
+          strokeLinecap="butt"
+          size={80}
+        />
+      )}
       <div className="text-stone-900 font-medium md:text-base dark:text-stone-100 mb-2">
         {label}
       </div>
-      <Prose>{explanation}</Prose>
+      {explanation && <Prose>{explanation}</Prose>}
+      {children}
     </Card>
   );
 }
--- homepage/homepage/components/home/ComingSoonSection.tsx
+++ homepage/homepage/components/home/ComingSoonSection.tsx
@@ -2,10 +2,9 @@ import CoPlainTextDescription from "@/app/(home)/coValueDescriptions/coPlainText
 import CursorsAndCaretsDescription from "@/app/(home)/toolkit/cursorsAndCarets.mdx";
 import TwoWaySyncDescription from "@/app/(home)/toolkit/twoWaySync.mdx";
 import VideoPresenceCallsDescription from "@/app/(home)/toolkit/videoPresenceCalls.mdx";
-import { Card } from "gcmp-design-system/src/app/components/atoms/Card";
 import { CodeRef } from "gcmp-design-system/src/app/components/atoms/CodeRef";
-import { H3 } from "gcmp-design-system/src/app/components/atoms/Headings";
 import { P } from "gcmp-design-system/src/app/components/atoms/Paragraph";
+import { FeatureCard } from "gcmp-design-system/src/app/components/molecules/FeatureCard";
 import { GappedGrid } from "gcmp-design-system/src/app/components/molecules/GappedGrid";
 import { Prose } from "gcmp-design-system/src/app/components/molecules/Prose";
 import { SectionHeader } from "gcmp-design-system/src/app/components/molecules/SectionHeader";
@@ -16,44 +15,39 @@ export function ComingSoonSection() {
       <SectionHeader title="More features coming soon" />
 
       <GappedGrid cols={4}>
-        <Card className="p-4">
-          <h3 className="text-stone-900 font-medium md:text-base dark:text-stone-100 mb-2">
-            Cursors & carets
-          </h3>
+        <FeatureCard className="p-4" label={<h3>Cursors & carets</h3>}>
           <P>Ready-made spatial presence.</P>
           <Prose size="sm">
             <CursorsAndCaretsDescription />
           </Prose>
-        </Card>
+        </FeatureCard>
 
-        <Card className="p-4">
-          <h3 className="text-stone-900 font-medium md:text-base dark:text-stone-100 mb-2">
-            Two-way sync to your DB
-          </h3>
+        <FeatureCard className="p-4" label={<h3>Two-way sync to your DB</h3>}>
           <P>Add Jazz to an existing app.</P>
           <Prose size="sm">
             <TwoWaySyncDescription />
           </Prose>
-        </Card>
+        </FeatureCard>
 
-        <Card className="p-4">
-          <h3 className="text-stone-900 font-medium md:text-base dark:text-stone-100 mb-2">
-            Video presence & calls
-          </h3>
+        <FeatureCard className="p-4" label={<h3>Video presence & calls</h3>}>
           <P>Stream and record audio & video.</P>
           <Prose size="sm">
             <VideoPresenceCallsDescription />
           </Prose>
-        </Card>
+        </FeatureCard>
 
-        <Card className="p-4">
-          <h3 className="text-stone-900 font-medium md:text-base dark:text-stone-100 mb-2">
-            <CodeRef>CoPlainText</CodeRef> & <CodeRef>CoRichText</CodeRef>
-          </h3>
+        <FeatureCard
+          className="p-4"
+          label={
+            <h3>
+              <CodeRef>CoPlainText</CodeRef> & <CodeRef>CoRichText</CodeRef>
+            </h3>
+          }
+        >
           <Prose size="sm">
             <CoPlainTextDescription />
           </Prose>
-        </Card>
+        </FeatureCard>
       </GappedGrid>
     </div>
   );
