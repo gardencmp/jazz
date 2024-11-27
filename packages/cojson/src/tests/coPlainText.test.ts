3cba5489bfee3b2b89c4f3726b3245e0ca1c2a10
Benjamin S. Leveritt <benjamin@leveritt.co.uk>
2024-11-27 11:56:47 +0000

    Fix tests, formatting

--- packages/cojson/src/tests/coPlainText.test.ts
+++ packages/cojson/src/tests/coPlainText.test.ts
@@ -1,73 +1,73 @@
 import { expect, test } from "vitest";
 import { expectPlainText } from "../coValue.js";
-import { WasmCrypto } from "../index.js";
+import { WasmCrypto } from "../crypto/WasmCrypto.js";
 import { LocalNode } from "../localNode.js";
 import { randomAnonymousAccountAndSessionID } from "./testUtils.js";
 
 const Crypto = await WasmCrypto.create();
 
 test("Empty CoPlainText works", () => {
-    const node = new LocalNode(...randomAnonymousAccountAndSessionID(), Crypto);
+  const node = new LocalNode(...randomAnonymousAccountAndSessionID(), Crypto);
 
-    const coValue = node.createCoValue({
-        type: "coplaintext",
-        ruleset: { type: "unsafeAllowAll" },
-        meta: null,
-        ...Crypto.createdNowUnique(),
-    });
+  const coValue = node.createCoValue({
+    type: "coplaintext",
+    ruleset: { type: "unsafeAllowAll" },
+    meta: null,
+    ...Crypto.createdNowUnique(),
+  });
 
-    const content = expectPlainText(coValue.getCurrentContent());
+  const content = expectPlainText(coValue.getCurrentContent());
 
-    expect(content.type).toEqual("coplaintext");
-    expect(content.toString()).toEqual("");
+  expect(content.type).toEqual("coplaintext");
+  expect(content.toString()).toEqual("");
 });
 
 test("Can insert into empty CoPlainText", () => {
-    const node = new LocalNode(...randomAnonymousAccountAndSessionID(), Crypto);
+  const node = new LocalNode(...randomAnonymousAccountAndSessionID(), Crypto);
 
-    const coValue = node.createCoValue({
-        type: "coplaintext",
-        ruleset: { type: "unsafeAllowAll" },
-        meta: null,
-        ...Crypto.createdNowUnique(),
-    });
+  const coValue = node.createCoValue({
+    type: "coplaintext",
+    ruleset: { type: "unsafeAllowAll" },
+    meta: null,
+    ...Crypto.createdNowUnique(),
+  });
 
-    const content = expectPlainText(coValue.getCurrentContent());
+  const content = expectPlainText(coValue.getCurrentContent());
 
-    expect(content.type).toEqual("coplaintext");
+  expect(content.type).toEqual("coplaintext");
 
-    content.insertAfter(0, "a", "trusting");
-    expect(content.toString()).toEqual("a");
+  content.insertAfter(0, "a", "trusting");
+  expect(content.toString()).toEqual("a");
 });
 
 test("Can insert and delete in CoPlainText", () => {
-    const node = new LocalNode(...randomAnonymousAccountAndSessionID(), Crypto);
+  const node = new LocalNode(...randomAnonymousAccountAndSessionID(), Crypto);
 
-    const coValue = node.createCoValue({
-        type: "coplaintext",
-        ruleset: { type: "unsafeAllowAll" },
-        meta: null,
-        ...Crypto.createdNowUnique(),
-    });
+  const coValue = node.createCoValue({
+    type: "coplaintext",
+    ruleset: { type: "unsafeAllowAll" },
+    meta: null,
+    ...Crypto.createdNowUnique(),
+  });
 
-    const content = expectPlainText(coValue.getCurrentContent());
+  const content = expectPlainText(coValue.getCurrentContent());
 
-    expect(content.type).toEqual("coplaintext");
+  expect(content.type).toEqual("coplaintext");
 
-    content.insertAfter(0, "hello", "trusting");
-    expect(content.toString()).toEqual("hello");
+  content.insertAfter(0, "hello", "trusting");
+  expect(content.toString()).toEqual("hello");
 
-    content.insertAfter(5, " world", "trusting");
-    expect(content.toString()).toEqual("hello world");
+  content.insertAfter(5, " world", "trusting");
+  expect(content.toString()).toEqual("hello world");
 
-    console.log("first delete")
-    content.deleteRange({from: 3, to: 8}, "trusting");
-    expect(content.toString()).toEqual("helrld");
+  console.log("first delete");
+  content.deleteRange({ from: 3, to: 8 }, "trusting");
+  expect(content.toString()).toEqual("helrld");
 
-    content.insertAfter(2, "😍", "trusting");
-    expect(content.toString()).toEqual("he😍lrld")
+  content.insertAfter(2, "😍", "trusting");
+  expect(content.toString()).toEqual("he😍lrld");
 
-    console.log("second delete")
-    content.deleteRange({from: 2, to: 4}, "trusting");
-    expect(content.toString()).toEqual("helrld");
-})
+  console.log("second delete");
+  content.deleteRange({ from: 2, to: 4 }, "trusting");
+  expect(content.toString()).toEqual("helrld");
+});
