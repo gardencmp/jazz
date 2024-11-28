8c6c671e630bd0921d5b9e58b6b892c41f7d8dc4
Benjamin S. Leveritt <benjamin@leveritt.co.uk>
2024-11-28 07:24:09 +0000

    Fix test formatting and running

--- packages/jazz-tools/src/tests/coRichText.test.ts
+++ packages/jazz-tools/src/tests/coRichText.test.ts
@@ -1,59 +1,66 @@
-import { expect, describe, test } from "vitest";
-import { Account, CoRichText, Marks, WasmCrypto } from "../index.js";
+import { describe, expect, test } from "vitest";
+import { Account, CoRichText, Group, Marks, WasmCrypto } from "../index.web.js";
 
 const Crypto = await WasmCrypto.create();
 
 describe("Simple CoRichText operations", async () => {
-    const me = await Account.create({
-        creationProps: { name: "Hermes Puggington" },
-        crypto: Crypto
+  const me = await Account.create({
+    creationProps: { name: "Hermes Puggington" },
+    crypto: Crypto,
+  });
+
+  const myGroup = await Group.create({
+    owner: me,
+  });
+
+  const text = CoRichText.createFromPlainText("hello world", {
+    owner: myGroup,
+  });
+
+  test("Construction", () => {
+    expect(text + "").toEqual("hello world");
+  });
+
+  describe("Mutation", () => {
+    test("insertion", () => {
+      const text = CoRichText.createFromPlainText("hello world", {
+        owner: me,
+      });
+
+      text.insertAfter(5, " cruel");
+      expect(text + "").toEqual("hello cruel world");
     });
 
-    const text = CoRichText.createFromPlainText("hello world", { owner: me });
+    test("deletion", () => {
+      const text = CoRichText.createFromPlainText("hello world", {
+        owner: me,
+      });
 
-    test("Construction", () => {
-        expect(text + "").toEqual("hello world");
+      text.deleteRange({ from: 3, to: 8 });
+      expect(text + "").toEqual("helrld");
     });
 
-    describe("Mutation", () => {
-        test("insertion", () => {
-            const text = CoRichText.createFromPlainText("hello world", {
-                owner: me,
-            });
+    test("inserting ranges", () => {
+      const text = CoRichText.createFromPlainText("hello world", {
+        owner: me,
+      });
 
-            text.insertAfter(5, " cruel");
-            expect(text + "").toEqual("hello cruel world");
-        });
+      text.insertMark(6, 9, Marks.Strong, { tag: "strong" });
 
-        test("deletion", () => {
-            const text = CoRichText.createFromPlainText("hello world", {
-                owner: me,
-            });
+      console.log(text.text?._raw.entries());
+      console.log(text.text?._raw.mapping);
 
-            text.deleteRange({from: 3, to: 8});
-            expect(text + "").toEqual("helrld");
-        });
-
-        test("inserting ranges", () => {
-            const text = CoRichText.createFromPlainText("hello world", {
-                owner: me,
-            });
-
-            text.insertMark(6, 9, Marks.Strong, { tag: "strong" });
-
-            console.log(text.text?._raw.entries());
-            console.log(text.text?._raw.mapping);
-
-            expect(text.resolveMarks()).toEqual([
-                {
-                    startAfter: 6,
-                    startBefore: 7,
-                    endAfter: 9,
-                    endBefore: 10,
-                    tag: "bold",
-                    from: text.marks![0],
-                },
-            ]);
-        });
+      expect(text.resolveMarks()).toEqual([
+        {
+          startAfter: 6,
+          startBefore: 7,
+          endAfter: 9,
+          endBefore: 10,
+          tag: "strong",
+          from: text.marks![0],
+          sourceMark: text.marks![0],
+        },
+      ]);
     });
+  });
 });
