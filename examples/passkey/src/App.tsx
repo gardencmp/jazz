88ffb58a4609b9faffa158186aed7ddbe344d5c6
Trisha Lim <hello@trishalim.com>
2024-11-28 14:18:07 +0000

    Clean up code on passkey example

--- examples/passkey/src/App.tsx
+++ examples/passkey/src/App.tsx
@@ -1,15 +1,14 @@
-import "./App.css";
 import { useAccount } from "./main";
 
 function App() {
   const { me, logOut } = useAccount();
 
   return (
-    <>
+    <main>
       <h1>You're logged in</h1>
-      <p>Welcome back, {me?.profile?.name}</p>
+      <p>Welcome back, {me?.profile?.name}!</p>
       <button onClick={() => logOut()}>Logout</button>
-    </>
+    </main>
   );
 }
 
