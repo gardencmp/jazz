296cef92f12134505d3125b34a308232886fdd64
Trisha Lim <hello@trishalim.com>
2024-11-28 11:32:13 +0000

    Implement simple image upload

--- examples/image-upload/src/App.tsx
+++ examples/image-upload/src/App.tsx
@@ -1,14 +1,21 @@
+import ImageUpload from "./ImageUpload.tsx";
 import { useAccount } from "./main";
 
 function App() {
   const { me, logOut } = useAccount();
 
   return (
-    <>
-      <h1>You're logged in</h1>
-      <p>Welcome back, {me?.profile?.name}</p>
-      <button onClick={() => logOut()}>Logout</button>
-    </>
+    <div className="container">
+      <nav>
+        <span>
+          You're logged in as <strong>{me?.profile?.name}</strong>
+        </span>
+        <button onClick={() => logOut()}>Logout</button>
+      </nav>
+      <main>
+        <ImageUpload />
+      </main>
+    </div>
   );
 }
 
