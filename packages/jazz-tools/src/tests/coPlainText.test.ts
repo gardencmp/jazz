64957b2d040b582e1e08a04ebaa13befca85e20a
Benjamin S. Leveritt <benjamin@leveritt.co.uk>
2024-11-28 07:24:09 +0000

    Fix test formatting and running

--- packages/jazz-tools/src/tests/coPlainText.test.ts
+++ packages/jazz-tools/src/tests/coPlainText.test.ts
@@ -1,33 +1,33 @@
-import { expect, describe, test } from "vitest";
-import { Account, CoPlainText, WasmCrypto } from "../index.js";
+import { describe, expect, test } from "vitest";
+import { Account, CoPlainText, WasmCrypto } from "../index.web.js";
 
 const Crypto = await WasmCrypto.create();
 
 describe("Simple CoPlainText operations", async () => {
-    const me = await Account.create({
-        creationProps: { name: "Hermes Puggington" },
-        crypto: Crypto
+  const me = await Account.create({
+    creationProps: { name: "Hermes Puggington" },
+    crypto: Crypto,
+  });
+
+  const text = CoPlainText.create("hello world", { owner: me });
+
+  test("Construction", () => {
+    expect(text + "").toEqual("hello world");
+  });
+
+  describe("Mutation", () => {
+    test("insertion", () => {
+      const text = CoPlainText.create("hello world", { owner: me });
+
+      text.insertAfter(5, " cruel");
+      expect(text + "").toEqual("hello cruel world");
     });
 
-    const text = CoPlainText.create("hello world", { owner: me });
+    test("deletion", () => {
+      const text = CoPlainText.create("hello world", { owner: me });
 
-    test("Construction", () => {
-        expect(text + "").toEqual("hello world");
+      text.deleteRange({ from: 3, to: 8 });
+      expect(text + "").toEqual("helrld");
     });
-
-    describe("Mutation", () => {
-        test("insertion", () => {
-            const text = CoPlainText.create("hello world", { owner: me });
-
-            text.insertAfter(5, " cruel");
-            expect(text + "").toEqual("hello cruel world");
-        });
-
-        test("deletion", () => {
-            const text = CoPlainText.create("hello world", { owner: me });
-
-            text.deleteRange({from: 3, to: 8});
-            expect(text + "").toEqual("helrld");
-        });
-    });
+  });
 });
