90d9e1ca915d2cb26403f32f74144d263d56f6a9
Benjamin S. Leveritt <benjamin@leveritt.co.uk>
2024-12-02 20:11:24 +0000

    Format files

--- examples/file-share/tailwind.config.ts
+++ examples/file-share/tailwind.config.ts
@@ -2,11 +2,11 @@ import typography from '@tailwindcss/typography';
 import type { Config } from 'tailwindcss';
 
 export default {
-	content: ['./src/**/*.{html,js,svelte,ts}'],
+  content: ['./src/**/*.{html,js,svelte,ts}'],
 
-	theme: {
-		extend: {}
-	},
+  theme: {
+    extend: {}
+  },
 
-	plugins: [typography]
+  plugins: [typography]
 } satisfies Config;
--- examples/file-share/vite.config.ts
+++ examples/file-share/vite.config.ts
@@ -1,10 +1,10 @@
-import { defineConfig } from 'vitest/config';
 import { sveltekit } from '@sveltejs/kit/vite';
+import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
-	plugins: [sveltekit()],
+  plugins: [sveltekit()],
 
-	test: {
-		include: ['src/**/*.{test,spec}.{js,ts}']
-	}
+  test: {
+    include: ['src/**/*.{test,spec}.{js,ts}']
+  }
 });
