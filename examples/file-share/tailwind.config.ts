861d3adec9d98cdb67387a7a3c49e09859af3e6b
Benjamin S. Leveritt <benjamin@leveritt.co.uk>
2024-12-02 12:28:07 +0000

    Add sv created file share app

---
+++ examples/file-share/tailwind.config.ts
@@ -0,0 +1,12 @@
+import typography from '@tailwindcss/typography';
+import type { Config } from 'tailwindcss';
+
+export default {
+	content: ['./src/**/*.{html,js,svelte,ts}'],
+
+	theme: {
+		extend: {}
+	},
+
+	plugins: [typography]
+} satisfies Config;
