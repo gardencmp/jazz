6e23eab73dcbdd56ca13ae9c3ce7bc7d3e3c194b
Benjamin S. Leveritt <benjamin@leveritt.co.uk>
2024-12-02 20:14:03 +0000

    Wrap auth and provider around app

--- examples/file-share/src/routes/+layout.svelte
+++ examples/file-share/src/routes/+layout.svelte
@@ -1,6 +1,22 @@
 <script lang="ts">
-	import '../app.css';
-	let { children } = $props();
+  import { PasskeyAuthBasicUI, usePasskeyAuth } from 'jazz-svelte';
+  import '../app.css';
+  import { Provider } from '$lib/jazz';
+
+  let { children } = $props();
+  const auth = usePasskeyAuth({
+    appName: 'File Share'
+  });
 </script>
 
-{@render children()}
+<div class="fixed bottom-4 right-4">
+  <PasskeyAuthBasicUI state={auth.state} />
+</div>
+
+{#if auth.current}
+  <Provider auth={auth.current} peer="wss://cloud.jazz.tools/?key=file-share-example@gcmp.io">
+    <div class="min-h-screen bg-gray-100">
+      {@render children()}
+    </div>
+  </Provider>
+{/if}
