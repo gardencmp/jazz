863c2f883793ca955dc0df90dbbccd35f4c4ea7a
pax-k <ndreipoppa@gmail.com>
2024-12-04 13:06:33 +0200

    ix: ts-ignore types mismatch (ugly)

--- packages/jazz-nodejs/src/index.ts
+++ packages/jazz-nodejs/src/index.ts
@@ -24,7 +24,7 @@ export async function startWorker<Acc extends Account>({
 }): Promise<{ worker: Acc; done: () => void }> {
   const wsPeer: Peer = createWebSocketPeer({
     id: "upstream",
-    // @ts-ignore types mismatch
+    // @ts-expect-error types mismatch
     websocket: new WebSocket(peer),
     role: "server",
   });
@@ -60,7 +60,7 @@ export async function startWorker<Acc extends Account>({
 
       const wsPeer: Peer = createWebSocketPeer({
         id: "upstream",
-        // @ts-ignore types mismatch
+        // @ts-expect-error types mismatch
         websocket: new WebSocket(peer),
         role: "server",
       });
