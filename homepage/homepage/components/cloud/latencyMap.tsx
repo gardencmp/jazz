7bb834f3996b8837282c7906ec0a233af7a71a2e
Anselm <anselm.eickhoff@gmail.com>
2024-12-07 12:54:37 +0000

    Lint & format

--- homepage/homepage/components/cloud/latencyMap.tsx
+++ homepage/homepage/components/cloud/latencyMap.tsx
@@ -1,5 +1,5 @@
-import MapTooltip from "./mapTooltip";
 import { clsx } from "clsx";
+import MapTooltip from "./mapTooltip";
 import { pingColorThresholds } from "./pingColorThresholds";
 
 export const LatencyMap = () => {
@@ -8,21 +8,21 @@ export const LatencyMap = () => {
       <div className="relative xl:-mx-[10%] xl:w-[120%] aspect-[12/4]">
         <MapTooltip />
       </div>
-        <div className="absolute bottom-0 left-0 lg:bottom-8 flex flex-col md:gap-1">
-          {pingColorThresholds.map((t, i) => (
-            <div
-              key={t.ping}
-              className={clsx("flex items-center gap-1", {
-                "hidden sm:flex": i % 2 !== 0,
-              })}
-            >
-              <div className={"size-2 md:size-3 rounded-full " + t.bgClass}></div>
-              <div className="text-[9px] md:text-xs font-mono">
-                &lt;{t.ping}ms
-              </div>
+      <div className="absolute bottom-0 left-0 lg:bottom-8 flex flex-col md:gap-1">
+        {pingColorThresholds.map((t, i) => (
+          <div
+            key={t.ping}
+            className={clsx("flex items-center gap-1", {
+              "hidden sm:flex": i % 2 !== 0,
+            })}
+          >
+            <div className={"size-2 md:size-3 rounded-full " + t.bgClass}></div>
+            <div className="text-[9px] md:text-xs font-mono">
+              &lt;{t.ping}ms
             </div>
-          ))}
-        </div>
+          </div>
+        ))}
       </div>
+    </div>
   );
 };
