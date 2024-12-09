a0548ecb25cf6db8e0e5cc28117f3527b2a8e28b
Benjamin S. Leveritt <benjamin@leveritt.co.uk>
2024-12-09 07:21:55 +0000

    Add vitest

--- packages/jazz-svelte/vite.config.ts
+++ packages/jazz-svelte/vite.config.ts
@@ -1,6 +1,10 @@
-import { sveltekit } from "@sveltejs/kit/vite";
-import { defineConfig } from "vite";
+import { defineConfig } from 'vitest/config';
+import { sveltekit } from '@sveltejs/kit/vite';
 
 export default defineConfig({
   plugins: [sveltekit()],
+
+  test: {
+    include: ['src/**/*.{test,spec}.{js,ts}']
+  }
 });
