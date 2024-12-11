5b72d1f89b7736f7d168e47628869f9cb925e8e9
Trisha Lim <hello@trishalim.com>
2024-12-11 16:15:22 +0000

    Show drafts in home

--- examples/form/src/Orders.tsx
+++ examples/form/src/Orders.tsx
@@ -1,9 +1,9 @@
 import { useAccount } from "./main.tsx";
-import { BubbleTeaOrder } from "./schema.ts";
+import { BubbleTeaOrder, DraftBubbleTeaOrder } from "./schema.ts";
 
-function Order({ order }: { order: BubbleTeaOrder }) {
+function Order({ order }: { order: BubbleTeaOrder | DraftBubbleTeaOrder }) {
   return (
-    <a href={`/#/order/${order.id}`}>
+    <a href={`/#/order/${order.id}`} className="border p-3 block">
       <div>{order.id}</div>
       <div>{order.baseTea}</div>
     </a>
@@ -12,17 +12,25 @@ function Order({ order }: { order: BubbleTeaOrder }) {
 
 export function Orders() {
   const { me } = useAccount({
-    profile: { orders: [] },
+    profile: { orders: [], draftOrders: [] },
   });
 
   return (
     <>
-      <section>
-        <h1>Your orders 🧋</h1>
+      <section className="space-y-5">
+        <div className="space-y-3">
+          <h1 className="font-semibold pb-2 border-b mb-3">Your orders 🧋</h1>
+          {me?.profile?.orders.map((order) =>
+            order ? <Order key={order.id} order={order} /> : null,
+          )}
+        </div>
 
-        {me?.profile?.orders.map((order) =>
-          order ? <Order key={order.id} order={order} /> : null,
-        )}
+        <div className="space-y-3">
+          <h2 className="font-semibold pb-2 border-b mb-3">Drafts</h2>
+          {me?.profile?.draftOrders.map((order) =>
+            order ? <Order key={order.id} order={order} /> : null,
+          )}
+        </div>
       </section>
     </>
   );
