f3581d108643a6663755d475c8fe0c520e6a6755
Trisha Lim <hello@trishalim.com>
2024-12-13 12:12:54 +0000

    Fix form example build

--- examples/form/vite.config.ts
+++ examples/form/vite.config.ts
@@ -1,17 +1,8 @@
-import path from "path";
-import react from "@vitejs/plugin-react-swc";
+import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
-// https://vitejs.dev/config/
+// https://vite.dev/config/
 export default defineConfig({
   // @ts-ignore
   plugins: [react()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
-  build: {
-    minify: false,
-  },
 });
