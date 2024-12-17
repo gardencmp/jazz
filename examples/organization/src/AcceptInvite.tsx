dda7c243d7e550212035d61c75445b3c85dc7107
Trisha Lim <hello@trishalim.com>
2024-12-17 14:59:42 +0000

    Add new org to list after invite accept

--- examples/organization/src/AcceptInvite.tsx
+++ examples/organization/src/AcceptInvite.tsx
@@ -1,12 +1,32 @@
+import { ID } from "jazz-tools";
 import { useNavigate } from "react-router";
-import { useAcceptInvite } from "./main.tsx";
+import { useAcceptInvite, useAccount } from "./main.tsx";
 import { Organization } from "./schema.ts";
 
 export function AcceptInvite() {
   const navigate = useNavigate();
+  const { me } = useAccount({ root: { organizations: [] } });
+
+  const onAccept = (organizationId: ID<Organization>) => {
+    if (me?.root?.organizations) {
+      Organization.load(organizationId, me, []).then((organization) => {
+        if (organization) {
+          // avoid duplicates
+          const ids = me.root.organizations.map(
+            (organization) => organization?.id,
+          );
+          if (ids.includes(organizationId)) return;
+
+          me.root.organizations.push(organization);
+          navigate("/organizations/" + organizationId);
+        }
+      });
+    }
+  };
+
   useAcceptInvite({
     invitedObjectSchema: Organization,
-    onAccept: (organizationId) => navigate("/organizations/" + organizationId),
+    onAccept,
   });
 
   return <p>Accepting invite...</p>;
