99a614038db835a47a5c1095046010e71ebdf3e5
Trisha Lim <hello@trishalim.com>
2024-12-17 17:27:26 +0000

    Form styling

--- examples/organization/src/components/Heading.tsx
+++ examples/organization/src/components/Heading.tsx
@@ -3,9 +3,7 @@ import { OrganizationSelector } from "./OrganizationSelector.tsx";
 export function Heading({ text }: { text: string }) {
   return (
     <div className="flex justify-between">
-      <h1 className="text-3xl font-semibold">
-        <strong>{text}</strong>
-      </h1>
+      <h1 className="text-3xl font-semibold">{text}</h1>
 
       <OrganizationSelector />
     </div>
