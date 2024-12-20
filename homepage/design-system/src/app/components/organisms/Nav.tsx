4778ad90d749bd3c5cd992c4d877da9457bbe3ac
Trisha Lim <hello@trishalim.com>
2024-12-20 19:04:37 +0000

    test

--- homepage/design-system/src/app/components/organisms/Nav.tsx
+++ homepage/design-system/src/app/components/organisms/Nav.tsx
@@ -229,6 +229,7 @@ export function MobileNav({
           )}
 
           <div className="flex flex-wrap justify-end py-2 gap-x-3 gap-y-1 border-b">
+            !!!!!! TEST!!!!!
             {[{ title: "Home", href: "/" }, ...items]
               .filter((item) => !("icon" in item))
               .map((item, i) => (
